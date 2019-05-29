cef2e29b5069da8c61ec27cec9e93e0df332a06a
shobana-mcw <shobana@multicorewareinc.com>
2019-05-29 16:04:29 +0530

    Add support for Absolute Difference - Opencl arithmetic function.

--- include/rppi_arithmatic_and_logical_functions.h
+++ include/rppi_arithmatic_and_logical_functions.h
@@ -121,20 +121,39 @@ rppi_add_u8_pkd3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
 // Gpu function declarations
 
 RppStatus
-rppi_add_u8_pln1_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
+rppi_subtract_u8_pln1_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
                               RppiSize srcSize, RppPtr_t dstPtr,
                               RppHandle_t rppHandle );
 
 RppStatus
-rppi_add_u8_pln3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
+rppi_subtract_u8_pln3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
                               RppiSize srcSize, RppPtr_t dstPtr,
                               RppHandle_t rppHandle );
 
 RppStatus
-rppi_add_u8_pkd3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
+rppi_subtract_u8_pkd3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
+                              RppiSize srcSize, RppPtr_t dstPtr,
+                              RppHandle_t rppHandle );
+// --------------------
+// Absolute difference
+// --------------------
+
+// Gpu function declarations
+
+RppStatus
+rppi_absolute_difference_u8_pln1_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
                               RppiSize srcSize, RppPtr_t dstPtr,
                               RppHandle_t rppHandle );
 
+RppStatus
+rppi_absolute_difference_u8_pln3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
+                              RppiSize srcSize, RppPtr_t dstPtr,
+                              RppHandle_t rppHandle );
+
+RppStatus
+rppi_absolute_difference_u8_pkd3_gpu( RppPtr_t srcPtr1, RppPtr_t srcPtr2,
+                              RppiSize srcSize, RppPtr_t dstPtr,
+                              RppHandle_t rppHandle );
 
 // Dummy
 RppStatus
