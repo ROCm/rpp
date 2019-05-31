fc5ff3e0d7a1bc2883b26e4561eb500055477f00
shobana-mcw <shobana@multicorewareinc.com>
2019-05-31 12:12:39 +0530

    Revert "modified and reverified unit test for contrast"
    This reverts commit fea1d4d8a0e4343e9338f99b30a32dd35122bdbd.

--- src/imgaug/rppi_contrast.cpp
+++ src/imgaug/rppi_contrast.cpp
@@ -11,6 +11,34 @@
 #endif //backend
 
 RppStatus
+rppi_contrast_u8_pln1_host(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr,
+                            Rpp32u newMin, Rpp32u newMax)
+{
+    host_contrast<Rpp8u>(static_cast<Rpp8u*>(srcPtr), srcSize,
+                                    static_cast<Rpp8u*>(dstPtr), newMin , newMax, 1 );
+    return RPP_SUCCESS;
+
+}
+
+RppStatus
+rppi_contrast_u8_pln3_host(RppPtr_t srcPtr, RppiSize srcSize,RppPtr_t dstPtr,
+                            Rpp32u newMin, Rpp32u newMax)
+{
+    host_contrast<Rpp8u>(static_cast<Rpp8u*>(srcPtr), srcSize,
+                                    static_cast<Rpp8u*>(dstPtr), newMin ,newMax, 3 );
+    return RPP_SUCCESS;
+}
+
+RppStatus
+rppi_contrast_u8_pkd3_host(RppPtr_t srcPtr, RppiSize srcSize,RppPtr_t dstPtr,
+                            Rpp32u newMin, Rpp32u newMax)
+{
+    host_contrast<Rpp8u>(static_cast<Rpp8u*>(srcPtr), srcSize,
+                                    static_cast<Rpp8u*>(dstPtr), newMin ,newMax, 3 );
+    return RPP_SUCCESS;
+}
+
+RppStatus
 rppi_contrast_u8_pln1_gpu(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr,
                             Rpp32u newMin, Rpp32u newMax, RppHandle_t rppHandle)
 {
@@ -56,6 +84,8 @@ rppi_contrast_u8_pln3_gpu(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr,
 
 }
 
+
+
 RppStatus
 rppi_contrast_u8_pkd3_gpu(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr,
                             Rpp32u newMin, Rpp32u newMax, RppHandle_t rppHandle)
@@ -77,35 +107,4 @@ rppi_contrast_u8_pkd3_gpu(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr,
 
     return RPP_SUCCESS;
 
-}
-
-RppStatus
-rppi_contrast_u8_pln1_host(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr,
-                           Rpp32u newMin, Rpp32u newMax)
-{
-    host_contrast<Rpp8u>(static_cast<Rpp8u*>(srcPtr), srcSize, static_cast<Rpp8u*>(dstPtr),
-                         newMin, newMax,
-                         RPPI_CHN_PLANAR, 1);
-    return RPP_SUCCESS;
-
-}
-
-RppStatus
-rppi_contrast_u8_pln3_host(RppPtr_t srcPtr, RppiSize srcSize,RppPtr_t dstPtr,
-                           Rpp32u newMin, Rpp32u newMax)
-{
-    host_contrast<Rpp8u>(static_cast<Rpp8u*>(srcPtr), srcSize, static_cast<Rpp8u*>(dstPtr),
-                         newMin, newMax,
-                         RPPI_CHN_PLANAR, 3);
-    return RPP_SUCCESS;
-}
-
-RppStatus
-rppi_contrast_u8_pkd3_host(RppPtr_t srcPtr, RppiSize srcSize,RppPtr_t dstPtr,
-                           Rpp32u newMin, Rpp32u newMax)
-{
-    host_contrast<Rpp8u>(static_cast<Rpp8u*>(srcPtr), srcSize, static_cast<Rpp8u*>(dstPtr),
-                         newMin, newMax,
-                         RPPI_CHN_PACKED, 3);
-    return RPP_SUCCESS;
 }
