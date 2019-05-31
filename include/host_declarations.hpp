bfa740bc4166a3f50af717ae86a1e015c3452296
shobana-mcw <shobana@multicorewareinc.com>
2019-05-31 12:12:40 +0530

    Revert "added host declarations header"
    This reverts commit 9c02edb78e541ca2092731c7c453a78789a50050.

--- include/host_declarations.hpp
+++
@@ -1,5 +0,0 @@
-#define RPPABS(a)       ((a < 0) ? -(a) : a)
-#define RPPMIN2(a,b)    ((a < b) ? a : b)
-#define RPPMIN3(a,b,c)  ((a < b) && (a < c) ?  a : ((b < c) ? b : c))
-#define RPPMAX2(a,b)    ((a > b) ? a : b)
-#define RPPMAX3(a,b,c)  ((a > b) && (a > c) ?  a : ((b > c) ? b : c))
