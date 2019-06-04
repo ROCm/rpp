bd7564e9435d5093aa21984733d8238df31d1577
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2019-06-04 10:31:12 +0530

    [cl] Aritmetic add stuff

---
+++ src/imgaug/hipoc/hipoc_brightness_contrast.cpp
@@ -0,0 +1,32 @@
+
+hipError_t
+hip_brightness_contrast_cl_back()
+{
+    hipModule_t module;
+    hipModuleLoad(&module, "brightness_contrast.cl.o");
+    hipFunction_t function;
+    hipModuleGetFunction(&function, module, "brightness_contrast");
+
+    hipStream_t stream;
+    hipStreamCreate(&stream);
+
+    hipEvent_t  start = nullptr;
+    hipEvent_t stop  = nullptr;
+
+    void* config[] = {  HIP_LAUNCH_PARAM_BUFFER_POINTER, &argBuffer[0],
+                        HIP_LAUNCH_PARAM_BUFFER_SIZE, &size,
+                        HIP_LAUNCH_PARAM_END};
+
+    hipHccModuleLaunchKernel(function, gdims[0],
+                                       gdims[1], gdims[2],
+                                       ldims[0], ldims[1],
+                                       ldims[2], 0,
+                                       0,
+                                       nullptr,
+                                      (void**)&config,
+                                       start,
+                                       stop);
+
+    hipDeviceSynchronize();
+
+}
