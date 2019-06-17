3ef72906bd152da592cc2bf71aaf6e5ab763c99c
Abishek <abishek.genex@gmail.com>
2019-06-17 16:21:27 -0700

    renamed statistics_funcitons to statistical_functions

--- include/rppi_statistical_functions.h
+++ include/rppi_statistical_functions.h
@@ -1,5 +1,11 @@
 #ifndef RPPI_STATISTICAL_FUNCTIONS_H
 #define RPPI_STATISTICAL_FUNCTIONS_H
+#include "rppdefs.h"
+
+#include "rppdefs.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 
 #endif /* RPPI_STATISTICAL_FUNCTIONS_H */
