4375afb54e9f7135f5a60f7acba97532dba7575d
r-abishekmcw <abishek@multicorewareinc.com>
2019-07-22 23:50:30 -0700

    modify random_crop_letter_box for any dstSize greater than the cropsize + 2*borderWidth

--- src/imgaug/cpu/host_image_augmentations.hpp
+++ src/imgaug/cpu/host_image_augmentations.hpp
@@ -963,56 +963,63 @@ RppStatus random_crop_letterbox_host(T* srcPtr, RppiSize srcSize, T* dstPtr, Rpp
         return RPP_ERROR;
     }
 
-    RppiSize srcNewSize;
-    int xDiff = (int) x2 - (int) x1;
-    int yDiff = (int) y2 - (int) y1;
-    srcNewSize.width = (Rpp32u) RPPABS(xDiff);
-    srcNewSize.height = (Rpp32u) RPPABS(yDiff);
-    
-    T *srcNewPtr = (T *)calloc(channel * srcNewSize.height * srcNewSize.width, sizeof(T));
-    T *srcPtrTemp, *srcNewPtrTemp;
-    srcPtrTemp = srcPtr;
-    srcNewPtrTemp = srcNewPtr;
+    Rpp32u borderWidth = 3;
+
+    RppiSize srcSizeSubImage;
+    T* srcPtrSubImage;
+    compute_subimage_location_host(srcPtr, &srcPtrSubImage, srcSize, &srcSizeSubImage, x1, y1, x2, y2, chnFormat, channel);
+
+    RppiSize srcSizeSubImagePadded;
+    srcSizeSubImagePadded.height = srcSizeSubImage.height + (2 * borderWidth);
+    srcSizeSubImagePadded.width = srcSizeSubImage.width + (2 * borderWidth);
+    if (dstSize.height < srcSizeSubImagePadded.height || dstSize.width < srcSizeSubImagePadded.width)
+    {
+        return RPP_ERROR;
+    }
+
+    T *srcPtrCrop = (T *)calloc(channel * srcSizeSubImage.height * srcSizeSubImage.width, sizeof(T));
+    generate_crop_host(srcPtr, srcSize, srcPtrSubImage, srcSizeSubImage, srcPtrCrop, chnFormat, channel);
+
+    T *srcPtrCropPadded = (T *)calloc(channel * srcSizeSubImagePadded.height * srcSizeSubImagePadded.width, sizeof(T));
+    generate_evenly_padded_image_host(srcPtrCrop, srcSizeSubImage, srcPtrCropPadded, srcSizeSubImagePadded, chnFormat, channel);
+
+    T *srcPtrCropPaddedTemp, *dstPtrTemp;
+    srcPtrCropPaddedTemp = srcPtrCropPadded;
+    dstPtrTemp = dstPtr;
 
     if (chnFormat == RPPI_CHN_PLANAR)
     {
         for (int c = 0; c < channel; c++)
         {
-            srcPtrTemp += (c * srcSize.height * srcSize.width);
-            srcPtrTemp += ((RPPMIN2(y1, y2) * srcSize.width) + RPPMIN2(x1, x2));
-            for (int i = RPPMIN2(y1, y2); i < RPPMAX2(y1, y2); i++)
+            dstPtrTemp = dstPtr + (c * dstSize.height * dstSize.width);
+            for (int i = 0; i < srcSizeSubImagePadded.height; i++)
             {
-                for (int j = RPPMIN2(x1, x2); j < RPPMAX2(x1, x2); j++)
+                for (int j = 0; j < srcSizeSubImagePadded.width; j++)
                 {
-                    *srcNewPtrTemp = *srcPtrTemp;
-                    srcNewPtrTemp++;
-                    srcPtrTemp++;
+                    *dstPtrTemp = *srcPtrCropPaddedTemp;
+                    dstPtrTemp++;
+                    srcPtrCropPaddedTemp++;
                 }
-                srcPtrTemp += (srcSize.width - srcNewSize.width);
+                dstPtrTemp = dstPtrTemp + (dstSize.width - srcSizeSubImagePadded.width);
             }
-            srcPtrTemp = srcPtr;
         }
     }
     else if (chnFormat == RPPI_CHN_PACKED)
     {
-        Rpp32s elementsInRow = srcSize.width * channel;
-        srcPtrTemp += (RPPMIN2(y1, y2) * elementsInRow) + (RPPMIN2(x1, x2) * channel);
-        for (int i = RPPMIN2(y1, y2); i < RPPMAX2(y1, y2); i++)
+        for (int i = 0; i < srcSizeSubImagePadded.height; i++)
         {
-            for (int j = RPPMIN2(x1, x2); j < RPPMAX2(x1, x2); j++)
+            for (int j = 0; j < srcSizeSubImagePadded.width; j++)
             {
                 for (int c = 0; c < channel; c++)
                 {
-                    *srcNewPtrTemp = *srcPtrTemp;
-                    srcNewPtrTemp++;
-                    srcPtrTemp++;
+                    *dstPtrTemp = *srcPtrCropPaddedTemp;
+                    dstPtrTemp++;
+                    srcPtrCropPaddedTemp++;
                 }
             }
-            srcPtrTemp += ((srcSize.width - srcNewSize.width) * channel);
+            dstPtrTemp = dstPtrTemp + (channel * (dstSize.width - srcSizeSubImagePadded.width));
         }
     }
-
-    generate_evenly_padded_image_host(srcNewPtr, srcNewSize, dstPtr, dstSize, chnFormat, channel);
 
     return RPP_SUCCESS;
     
