c17427326633de1668faf0bca64f46610f8b2272
Muthukumaravel <muthukumaravel@multicorewareinc.com>
2019-08-06 16:50:13 +0530

    Modified location of API

--- include/rppi_statistical_operations.h
+++ include/rppi_statistical_operations.h
@@ -30,7 +30,7 @@ RppStatus
 rppi_histogram_u8_pkd3_host(RppPtr_t srcPtr, RppiSize srcSize, Rpp32u* outputHistogram, Rpp32u bins);
 
 // ----------------------------------------
-// Host integral functions declaration 
+// gpu integral functions declaration 
 // ----------------------------------------
 
 RppStatus
@@ -42,8 +42,32 @@ rppi_integral_u8_pln3_gpu(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr, Rp
 RppStatus
 rppi_integral_u8_pkd3_gpu(RppPtr_t srcPtr, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
 
+// ----------------------------------------
+// gpu min functions declaration 
+// ----------------------------------------
 
- 
+RppStatus
+rppi_min_u8_pln1_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
+
+RppStatus
+rppi_min_u8_pln3_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
+
+RppStatus
+rppi_min_u8_pkd3_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
+
+// ----------------------------------------
+// gpu max functions declaration 
+// ----------------------------------------
+
+RppStatus
+rppi_max_u8_pln1_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
+
+RppStatus
+rppi_max_u8_pln3_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
+
+RppStatus
+rppi_max_u8_pkd3_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RppiSize srcSize, RppPtr_t dstPtr, RppHandle_t rppHandle);
+
 #ifdef __cplusplus
 }
 #endif
