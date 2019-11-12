d4eeb558ca323ba00a95b09318b39f457ca3ba18
rrawther <Rajy.MeeyakhanRawther@amd.com>
2019-11-12 14:03:15 -0800

    resize simd routine added

--- src/include/cpu/rpp_cpu_common.hpp
+++ src/include/cpu/rpp_cpu_common.hpp
@@ -20,7 +20,13 @@
 #define RPPPIXELCHECK(pixel)    (pixel < (Rpp32f) 0) ? ((Rpp32f) 0) : ((pixel < (Rpp32f) 255) ? pixel : ((Rpp32f) 255))
 
 #if ENABLE_SIMD_INTRINSICS
-
+#if _WIN32
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#include <smmintrin.h>
+#include <immintrin.h>
+#endif
 #define M256I(m256i_register) (*((_m256i_union*)&m256i_register))
 typedef union {
     char               m256i_i8[32];
@@ -567,6 +573,7 @@ inline RppStatus resize_kernel_host(T* srcPtr, RppiSize srcSize, T* dstPtr, Rppi
 }
 
 #if ENABLE_SIMD_INTRINSICS
+#define CHECK_SIMD      0
 
 #define FP_BITS     16
 #define FP_MUL      (1<<FP_BITS)
@@ -583,12 +590,15 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
     // call ref host implementation
     //if (channel > 3 )
     //    return resize_kernel_host(srcPtr, dstPtr, dstSize, chnFormat, channel);
+#if CHECK_SIMD
+    Rpp8u *tmpBuff = new Rpp8u[dstSize.width*dstSize.height*3];
+#endif
 
     Rpp8u *srcPtrTemp, *dstPtrTemp;
     srcPtrTemp = srcPtr;
     dstPtrTemp = dstPtr;
-    Rpp32f yscale = ((Rpp32f) (dstSize.height)) / ((Rpp32f) (srcSize.height));    // yscale
-    Rpp32f xscale = ((Rpp32f) (dstSize.width)) / ((Rpp32f) (srcSize.width));      //xscale
+    Rpp32f yscale = ((Rpp32f) (srcSize.height)) / ((Rpp32f) (dstSize.height));    // yscale
+    Rpp32f xscale = ((Rpp32f) (srcSize.width)) / ((Rpp32f) (dstSize.width));      //xscale
     int alignW = (dstSize.width + 15) & ~15;
     // generate maps for fixed point computations
     unsigned int *Xmap = new unsigned int[alignW*2];
@@ -656,10 +666,6 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
                     if (p11 > pSrcBorder) p11 = pSrcBorder;
                     result = ((Xf1[x] * fy1*p0[0]) + (Xf[x] * fy1*p01[0]) + (Xf1[x] * fy*p1[0]) + (Xf[x] * fy*p11[0]) + 0x8000) >> 16;
                     *pdst++ = (Rpp8u) std::max(0, std::min(result, 255));
-                    result = ((Xf1[x] * fy1*p0[1]) + (Xf[x] * fy1*p01[1]) + (Xf1[x] * fy*p1[1]) + (Xf[x] * fy*p11[1]) + 0x8000) >> 16;
-                    *pdst++ = (Rpp8u)std::max(0, std::min(result, 255));
-                    result = ((Xf1[x] * fy1*p0[2]) + (Xf[x] * fy1*p01[2]) + (Xf1[x] * fy*p1[2]) + (Xf[x] * fy*p11[2]) + 0x8000) >> 16;
-                    *pdst++ = (Rpp8u)std::max(0, std::min(result, 255));
                 }
             }
             srcPtrTemp += srcSize.height * srcSize.width;
@@ -676,12 +682,11 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
 #if __AVX2__
         const __m256i mm_zeros = _mm256_setzero_si256();
         const __m256i mm_round = _mm256_set1_epi32((int)0x80);
-        const __m256i pmask1 = _mm256_set_epi32(0, 1, 2, 4, 5, 6, 3, 7);
-        const __m256i pmask2 = _mm256_set_epi32(2, 3, 4, 5, 6, 7, 0, 1);
+        const __m256i pmask1 = _mm256_setr_epi32(0, 1, 4, 2, 3, 6, 5, 7);
+        const __m256i pmask2 = _mm256_setr_epi32(0, 1, 4, 5, 2, 3, 6, 7);
 #endif
         int ypos = (int)(FP_MUL * (yscale*0.5 - 0.5));
-//#pragma omp parallel for
-       for (int y = 0; y < (int)dstSize.height; y++, ypos += yinc)
+        for (int y = 0; y < (int)dstSize.height; y++, ypos += yinc)
         {
             int ym, fy, fy1;
             Rpp8u *pSrc1, *pSrc2;
@@ -700,8 +705,8 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
             }
             unsigned int x = 0;
 #if __AVX2__
-            
-            __m256i w_y = _mm256_set_epi32(fy1, fy, fy1, fy, fy1, fy, fy1, fy);
+
+            __m256i w_y = _mm256_setr_epi32(fy1, fy, fy1, fy, fy1, fy, fy1, fy);
             __m256i p01, p23, ps01, ps23, px0, px1, ps2, ps3;
             for (; x < aligned_width; x += 4)
             {
@@ -721,8 +726,10 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
                 p23 = _mm256_unpacklo_epi8(p23, ps23);
                 p01 = _mm256_unpacklo_epi16(p01, _mm256_srli_si256(p01, 6));     //R0R1R2R3 G0G1G2G3 B0B1B2B3 XXXX for 1st and 3rd pixel
                 p23 = _mm256_unpacklo_epi16(p23, _mm256_srli_si256(p23, 6));      //R0R1R2R3 G0G1G2G3 B0B1B2B3 XXXX for 2nd and 4th pixel
+                p01 = _mm256_permutevar8x32_epi32(p01, pmask2); // R0,G0, B0, R1, G1, B1, xx, xx
+                p23 = _mm256_permutevar8x32_epi32(p23, pmask2); // R2, G2, B2, R3, G3, B3, xx, xx,
 
-                // load xf and 1-xf
+                // load and get the weights in place
                 ps01 = _mm256_setr_epi32(Xf1[x], Xf1[x], Xf[x], Xf[x], Xf1[x+2], Xf1[x+2], Xf[x+2], Xf[x+2]);            // xfxfxf1xf1
                 ps23 = _mm256_setr_epi32(Xf1[x+1], Xf1[x+1], Xf[x+1], Xf[x+1], Xf1[x+3], Xf1[x+3], Xf[x+3], Xf[x+3]);
                 ps01 = _mm256_mullo_epi32(ps01, w_y);                      // W0W1W2W3 for 1st and 3rd
@@ -746,17 +753,18 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
                 px1 = _mm256_madd_epi16(px1, ps23);                  // pix1: (W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3), (W0*B0+W1*B1), (W2*B2+W3*B3), xx, xx 
                 p01 = _mm256_madd_epi16(p01, ps2);                  // pix2: (W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3), (W0*B0+W1*B1), (W2*B2+W3*B3), xx, xx 
                 p23 = _mm256_madd_epi16(p23, ps3);                  // pix3: (W0*R0+W1*R1), (W2*R2+W3*R3), (W0*G0+W1*G1), (W2*G2+W3*G3), (W0*B0+W1*B1), (W2*B2+W3*B3), xx, xx 
-                px0 = _mm256_hadd_epi32(px0, px1);      // R0,G0, B0, xx, R1, G1, B1, xx (32bit)
-                p01 = _mm256_hadd_epi32(p01, p23);      // R2,G2, B2, xx, R3, G3, B3, xx (32bit)
+                px0 = _mm256_hadd_epi32(px0, px1);      // R0,G0, R1, G1, B0, xx, B1, xx (32bit)
+                p01 = _mm256_hadd_epi32(p01, p23);      // R2,G2, R3, G3, B2, xx, B3, xx (32bit)
                 px0 = _mm256_permutevar8x32_epi32(px0, pmask1); // R0,G0, B0, R1, G1, B1, xx, xx 
                 p01 = _mm256_permutevar8x32_epi32(p01, pmask1); // R2, G2, B2, R3, G3, B3, xx, xx, 
                 px0 = _mm256_add_epi32(px0, mm_round);
                 p01 = _mm256_add_epi32(p01, mm_round);
                 px0 = _mm256_srli_epi32(px0, 8);      // /256
-                p01 = _mm256_srli_epi32(px0, 8);      // /256
-                px0 = _mm256_packus_epi32(px0, p01); //R0G0B0R1G1B1xx R2G2B2R3B3G3xx
-                px0 = _mm256_permutevar8x32_epi32(px0, pmask1); //R0G0B0R1G1B1R2G2B2R3B3G3xxxx
-                px0 = _mm256_packus_epi16(px0, mm_zeros); //R0G0B0R1G1B1R2G2B2R3G3B3xxxx ....
+                p01 = _mm256_srli_epi32(p01, 8);      // /256
+                px0 = _mm256_packus_epi32(px0, p01); //R0G0B0R1 R2G2B2R3 G1B1xx B3G3xx
+                p01 = _mm256_permutevar8x32_epi32(px0, pmask1); //R0G0B0R1G1B1R2G2B2R3B3G3xxxx
+                px0 = _mm256_permute4x64_epi64(p01, 0xaa);
+                px0 = _mm256_packus_epi16(p01, px0); //R0G0B0R1G1B1R2G2B2R3G3B3xxxx ....
                 _mm_storeu_si128((__m128i *)pdst, M256I(px0).m256i_i128[0]);      // write 12 bytes
                 pdst += 12;
             }
@@ -778,9 +786,35 @@ inline RppStatus resize_kernel_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstP
                 result = ((Xf1[x] * fy1*p0[2]) + (Xf[x] * fy1*p01[2]) + (Xf1[x] * fy*p1[2]) + (Xf[x] * fy*p11[2]) + 0x8000) >> 16;
                 *pdst++ = (Rpp8u)std::max(0, std::min(result, 255));
             }
+#if CHECK_SIMD
+            Rpp8u *pdstRef =  dstPtrTemp + y*dstride;
+            Rpp8u *tmpRef = tmpBuff + y*dstride;
+            for (x=0; x < dstSize.width; x++) {
+                int result;
+                const unsigned char *p0 = pSrc1 + Xmap[x];
+                const unsigned char *p01 = p0 + channel;
+                const unsigned char *p1 = pSrc2 + Xmap[x];
+                const unsigned char *p11 = p1 + channel;
+                if (p0 > pSrcBorder) p0 = pSrcBorder;
+                if (p1 > pSrcBorder) p1 = pSrcBorder;
+                if (p01 > pSrcBorder) p01 = pSrcBorder;
+                if (p11 > pSrcBorder) p11 = pSrcBorder;
+                result = ((Xf1[x] * fy1*p0[0]) + (Xf[x] * fy1*p01[0]) + (Xf1[x] * fy*p1[0]) + (Xf[x] * fy*p11[0]) + 0x8000) >> 16;
+                tmpRef[x*3] = (Rpp8u) std::max(0, std::min(result, 255));
+                result = ((Xf1[x] * fy1*p0[1]) + (Xf[x] * fy1*p01[1]) + (Xf1[x] * fy*p1[1]) + (Xf[x] * fy*p11[1]) + 0x8000) >> 16;
+                tmpRef[x*3+1] = (Rpp8u)std::max(0, std::min(result, 255));
+                result = ((Xf1[x] * fy1*p0[2]) + (Xf[x] * fy1*p01[2]) + (Xf1[x] * fy*p1[2]) + (Xf[x] * fy*p11[2]) + 0x8000) >> 16;
+                tmpRef[x*3+2] = (Rpp8u)std::max(0, std::min(result, 255));
+                if ( y==0 && (pdstRef[x*3] != tmpRef[x*3] || pdstRef[x*3+1] != tmpRef[x*3+1] || pdstRef[x*3+2] != tmpRef[x*3+2]))
+                    printf("Error: pixel mismatch at %d: %x %x %x != %x %x %x(ref)\n", x, pdstRef[x*3], pdstRef[x*3+1], pdstRef[x*3+2], tmpRef[x*3], tmpRef[x*3+1], tmpRef[x*3+2]);
+            }
+#endif
         }
     }
     if (Xmap) delete[] Xmap;
+#if CHECK_SIMD
+    delete [] tmpBuff;
+#endif
 
     return RPP_SUCCESS;
 }
