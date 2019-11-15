7ad966e167f438b39a872222d818c5779a2488d7
Reza-Najafi <Seyedreza.Najafi@amd.com>
2019-11-15 12:29:02 -0800

    Added SIMD accelerated flip

---
+++ src/imgaug/cpu/host_geometry_transforms_simd.hpp
@@ -0,0 +1,206 @@
+//
+// Created by svcbuild on 11/15/19.
+//
+
+#ifndef AMD_RPP_HOST_GEOMETRY_TRANSFORMS_SIMD_HPP
+#define AMD_RPP_HOST_GEOMETRY_TRANSFORMS_SIMD_HPP
+#include <cpu/rpp_cpu_common.hpp>
+
+#if ENABLE_SIMD_INTRINSICS
+
+/**************** Flip ***************/
+
+template <>
+RppStatus flip_host(Rpp8u* srcPtr, RppiSize srcSize, Rpp8u* dstPtr,
+                    RppiAxis flipAxis,
+                    RppiChnFormat chnFormat, Rpp32u channel)
+{
+    Rpp8u *srcPtrTemp, *dstPtrTemp;
+    srcPtrTemp = srcPtr;
+    dstPtrTemp = dstPtr;
+
+    if (chnFormat == RPPI_CHN_PLANAR)
+    {
+       int alignW = (srcSize.width + 31) & ~31;
+       if (flipAxis == RPPI_HORIZONTAL_AXIS)
+        {
+            for (int c = 0; c < channel; c++)
+            {
+                srcPtrTemp = srcPtr + ((c + 1) * srcSize.height * srcSize.width) - srcSize.width;
+                for (int i = 0; i < srcSize.height; i++)
+                {
+                    int j=0;
+#if __AVX2__
+                    __m256i p0;
+                    for (; j < alignW; j+=32) {
+                        p0 = _mm256_loadu_si256((const __m256i*)srcPtrTemp);
+                        _mm256_storeu_si256((__m256i *)dstPtrTemp, p0);
+                        srcPtrTemp +=32, dstPtrTemp += 32;
+                    }
+#endif
+                    for (; j < srcSize.width; j++)
+                    {
+                        *dstPtrTemp++ = *srcPtrTemp++;
+                    }
+                    srcPtrTemp = srcPtrTemp - (2 * srcSize.width);
+                }
+            }
+        }
+        else if (flipAxis == RPPI_VERTICAL_AXIS)
+        {
+            for (int c = 0; c < channel; c++)
+            {
+                srcPtrTemp = srcPtr + (c * srcSize.height * srcSize.width) + srcSize.width;
+                for (int i = 0; i < srcSize.height; i++)
+                {
+                    int j=0;
+#if __AVX2__
+                    __m256i p0;
+                    for (; j < alignW; j+=32) {
+                        srcPtrTemp -= 32;
+                        p0 = _mm256_loadu_si256((const __m256i*)srcPtrTemp);
+                        _mm256_storeu_si256((__m256i *)dstPtrTemp, p0);
+                        dstPtrTemp += 32;
+                    }
+#endif
+                    for (; j < srcSize.width; j++)
+                    {
+                        srcPtrTemp--;
+                        *dstPtrTemp++ = *srcPtrTemp;
+                    }
+                    srcPtrTemp = srcPtrTemp + (2 * srcSize.width);
+                }
+            }
+        }
+        else if (flipAxis == RPPI_BOTH_AXIS)
+        {
+            for (int c = 0; c < channel; c++)
+            {
+                srcPtrTemp = srcPtr + ((c + 1) * srcSize.height * srcSize.width);
+                for (int i = 0; i < srcSize.height; i++)
+                {
+                    int j=0;
+#if __AVX2__
+                    __m256i p0;
+                    for (; j < alignW; j+=32) {
+                        srcPtrTemp -= 32;
+                        p0 = _mm256_loadu_si256((const __m256i*)srcPtrTemp);
+                        _mm256_storeu_si256((__m256i *)dstPtrTemp, p0);
+                        dstPtrTemp += 32;
+                    }
+
+#endif
+                    for (; j < srcSize.width; j++)
+                    {
+                        srcPtrTemp--;
+                        *dstPtrTemp++ = *srcPtrTemp;
+                        dstPtrTemp++;
+                    }
+                }
+            }
+        }
+    }
+    else if (chnFormat == RPPI_CHN_PACKED)
+    {
+        //todo:: if channel is 3, call reference
+        int alignW = srcSize.width / 10; // proces 10 pixels in one load
+        if (flipAxis == RPPI_HORIZONTAL_AXIS)
+        {
+            srcPtrTemp = srcPtr + (channel * ((srcSize.height-1) * srcSize.width));
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp8u* pSrc = srcPtrTemp;
+                int j=0;
+#if __AVX2__
+                __m256i p0;
+                // copy multiple of 3 bytes for 3 channels
+                for (; j < alignW; j+=10) {
+                    p0 = _mm256_loadu_si256((const __m256i*)pSrc);
+                    _mm256_storeu_si256((__m256i *)dstPtrTemp, p0);
+                    pSrc +=30, dstPtrTemp += 30;
+                }
+
+                for (; j < srcSize.width; j++)
+                {
+                    *dstPtrTemp++ = pSrc[0];
+                    *dstPtrTemp++ = pSrc[1];
+                    *dstPtrTemp++ = pSrc[2];
+                    pSrc += 3;
+                }
+                srcPtrTemp -= srcSize.width*channel;
+#endif
+                for (; j < srcSize.width; j++)
+                {
+                    for (int c = 0; c < channel; c++)
+                    {
+                        *dstPtrTemp = *srcPtrTemp;
+                        dstPtrTemp++;
+                        srcPtrTemp++;
+                    }
+                }
+
+
+                srcPtrTemp = srcPtrTemp - (channel * (2 * srcSize.width));
+            }
+        }
+        else if (flipAxis == RPPI_VERTICAL_AXIS)
+        {
+            srcPtrTemp = srcPtr + channel * srcSize.width;
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp8u* pSrc = srcPtrTemp;
+                int j=0;
+#if __AVX2__
+                __m256i p0;
+                // copy multiple of 3 bytes for 3 channels
+                for (; j < alignW; j+=10) {
+                    pSrc -= 30;
+                    p0 = _mm256_loadu_si256((const __m256i*)pSrc);
+                    _mm256_storeu_si256((__m256i *)dstPtrTemp, p0);
+                    dstPtrTemp += 30;
+                }
+#endif
+                for (; j < srcSize.width; j++)
+                {
+                    pSrc -= 3;
+                    *dstPtrTemp++ = pSrc[0];
+                    *dstPtrTemp++ = pSrc[1];
+                    *dstPtrTemp++ = pSrc[2];
+                }
+                srcPtrTemp += srcSize.width*channel;
+            }
+        }
+        else if (flipAxis == RPPI_BOTH_AXIS)
+        {
+            srcPtrTemp = srcPtr + (channel * (srcSize.height * srcSize.width));
+            for (int i = 0; i < srcSize.height; i++)
+            {
+                Rpp8u* pSrc = srcPtrTemp;
+                int j=0;
+#if __AVX2__
+                __m256i p0;
+                // copy multiple of 3 bytes for 3 channels
+                for (; j < alignW; j+= 10) {
+                    pSrc -= 30;
+                    p0 = _mm256_loadu_si256((const __m256i*)pSrc);
+                    _mm256_storeu_si256((__m256i *)dstPtrTemp, p0);
+                    dstPtrTemp += 30;
+                }
+#endif
+                for (; j < srcSize.width; j++)
+                {
+                    pSrc -= 3;
+                    *dstPtrTemp++ = pSrc[0];
+                    *dstPtrTemp++ = pSrc[1];
+                    *dstPtrTemp++ = pSrc[2];
+                }
+                srcPtrTemp -= srcSize.width*channel;
+            }
+        }
+    }
+
+    return RPP_SUCCESS;
+}
+
+#endif
+#endif //AMD_RPP_HOST_GEOMETRY_TRANSFORMS_SIMD_HPP
