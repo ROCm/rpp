0dd071752b5d544fa9cadf0cab19fbc70cf4eeeb
LokeshBonta <lokeswara@multicorewareinc.com>
2020-09-30 22:58:17 +0530

    convert bit depth for both image and tensor has been added

--- include/rppi_arithmetic_operations.h
+++ include/rppi_arithmetic_operations.h
@@ -2516,6 +2516,45 @@ rppi_tensor_table_look_up_u8_gpu(RppPtr_t srcPtr, RppPtr_t dstPtr, Rpp8u *look_u
 RppStatus
 rppi_tensor_convert_bit_depth_u8_gpu(RppPtr_t srcPtr, RppPtr_t dstPtr, RppConvertBitDepthMode convert_mode, Rpp32u tensorDimension, RppPtr_t tensorDimensionValues, rppHandle_t rppHandle);
 
+// ----------------------------------------
+// GPU Image Bit Depth functions declaration 
+// ----------------------------------------
+/* Does convert bit detpth for an image.
+*param[in] srcPtr  input image
+*param[in] srcSize dimensions of the image
+*param[out] dstPtr output image
+*returns a  RppStatus enumeration. 
+*retval RPP_SUCCESS : No error succesful completion
+*retval RPP_ERROR : Error 
+*/
+RppStatus
+ rppi_image_bit_depth_u8_pln1_gpu(RppPtr_t srcPtr ,RppiSize srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pln1_batchSS_gpu(RppPtr_t srcPtr ,RppiSize srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pln1_batchDS_gpu(RppPtr_t srcPtr ,RppiSize *srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pln1_batchPS_gpu(RppPtr_t srcPtr ,RppiSize *srcSize ,RppiSize maxSrcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+
+RppStatus
+ rppi_image_bit_depth_u8_pln3_gpu(RppPtr_t srcPtr ,RppiSize srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pln3_batchSS_gpu(RppPtr_t srcPtr ,RppiSize srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pln3_batchDS_gpu(RppPtr_t srcPtr ,RppiSize *srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pln3_batchPS_gpu(RppPtr_t srcPtr ,RppiSize *srcSize ,RppiSize maxSrcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+
+RppStatus
+ rppi_image_bit_depth_u8_pkd3_gpu(RppPtr_t srcPtr ,RppiSize srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pkd3_batchSS_gpu(RppPtr_t srcPtr ,RppiSize srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pkd3_batchDS_gpu(RppPtr_t srcPtr ,RppiSize *srcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+RppStatus
+ rppi_image_bit_depth_u8_pkd3_batchPS_gpu(RppPtr_t srcPtr ,RppiSize *srcSize ,RppiSize maxSrcSize ,RppPtr_t dstPtr , RppConvertBitDepthMode convert_mode, Rpp32u nbatchSize ,rppHandle_t rppHandle );
+
+
 #ifdef __cplusplus
 }
 #endif
