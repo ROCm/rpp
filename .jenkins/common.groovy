d71b2ab4fa62bf9d43e8cfc0329319f7a3fb358b
Kiriti Gowda <kiriti.nageshgowda@amd.com>
2021-01-30 12:44:50 -0800

    Jenkins Infrastructure (#41)
    * Jenkins Infrastructure
    
    * Jenkin Runners
    
    * Jenkins - Test Added
    
    * Jenkins Cleanup
    
    Co-authored-by: saadrahim <44449863+saadrahim@users.noreply.github.com>

---
+++ .jenkins/common.groovy
@@ -0,0 +1,97 @@
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+def runCompileCommand(platform, project, jobName, boolean debug=false, boolean staticLibrary=false) {
+    project.paths.construct_build_prefix()
+
+    String buildTypeArg = debug ? '-DCMAKE_BUILD_TYPE=Debug' : '-DCMAKE_BUILD_TYPE=Release'
+    String buildTypeDir = debug ? 'debug' : 'release'
+
+    String osInfo = ""
+    String update = ""
+    String installPackage = ""
+    String cmake = ""
+
+    if (platform.jenkinsLabel.contains('centos'))
+    {
+        osInfo = 'cat /etc/os-release && uname -r'
+        update = 'sudo yum -y update'
+        installPackage = 'sudo yum install -y boost-devel clang'
+        cmake = 'cmake3'
+    }
+    else
+    {
+        osInfo = 'cat /etc/lsb-release && uname -r'
+        update = 'sudo apt -y update'
+        installPackage = 'sudo apt install -y unzip cmake libboost-all-dev clang'
+        cmake = 'cmake'
+    }
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                ${osInfo}
+                ${update}
+                echo Install RPP Prerequisites
+                mkdir -p rpp-deps && cd rpp-deps
+                ${installPackage}
+                wget https://sourceforge.net/projects/half/files/half/1.12.0/half-1.12.0.zip
+                unzip half-1.12.0.zip -d half-files
+                sudo cp half-files/include/half.hpp /usr/local/include/
+                cd ../
+                echo Build RPP - ${buildTypeDir}
+                cd ${project.paths.project_build_prefix}
+                mkdir -p build/${buildTypeDir} && cd build/${buildTypeDir}
+                ${cmake} -DBACKEND=OCL ${buildTypeArg} ../..
+                make -j\$(nproc)
+                sudo make install
+                sudo make package
+                """
+    
+    platform.runCommand(this, command)
+}
+
+def runTestCommand (platform, project) {
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                ldd -v /opt/rocm/rpp/lib/libamd_rpp.so
+                """
+
+    platform.runCommand(this, command)
+    // Unit tests - TBD
+}
+
+def runPackageCommand(platform, project) {
+
+    def packageHelper = platform.makePackage(platform.jenkinsLabel, "${project.paths.project_build_prefix}/build/release")
+
+    String packageType = ""
+    String packageInfo = ""
+
+    if (platform.jenkinsLabel.contains('centos'))
+    {
+        packageType = 'rpm'
+        packageInfo = 'rpm -qlp'
+    }
+    else
+    {
+        packageType = 'deb'
+        packageInfo = 'dpkg -c'
+    }
+
+    def command = """#!/usr/bin/env bash
+                set -x
+                export HOME=/home/jenkins
+                echo Make RPP Package
+                cd ${project.paths.project_build_prefix}/build/release
+                sudo make package
+                mkdir -p package
+                mv *.${packageType} package/
+                ${packageInfo} package/*.${packageType}
+                """
+
+    platform.runCommand(this, command)
+    platform.archiveArtifacts(this, packageHelper[1])
+}
+
+return this
