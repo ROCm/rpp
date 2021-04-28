1ddd6a1e9a532cd5abed0437ea44fe58b49a64fa
Abishek <52214183+r-abishek@users.noreply.github.com>
2021-04-28 11:07:34 -0700

    static build option - statistical_operations enabled (#52)
    * Mods for max/min/thresholding
    
    * Merge branch 'ar/master' into ar/hip_stat_opt
    
    * Remove fork CI

--- src/modules/hip/kernel/rpp_hip_host_decls.hpp
+++ src/modules/hip/kernel/rpp_hip_host_decls.hpp
@@ -67,5 +67,9 @@ RppStatus hip_exec_inclusive_OR_batch(Rpp8u *srcPtr1, Rpp8u *srcPtr2, Rpp8u *dst
 
 // statistical_operations
 
+RppStatus hip_exec_thresholding_batch(Rpp8u *srcPtr, Rpp8u *dstPtr, rpp::Handle& handle, RppiChnFormat chnFormat, Rpp32u channel, Rpp32s plnpkdind, Rpp32u max_height, Rpp32u max_width);
+RppStatus hip_exec_min_batch(Rpp8u *srcPtr1, Rpp8u *srcPtr2, Rpp8u *dstPtr, rpp::Handle& handle, RppiChnFormat chnFormat, Rpp32u channel, Rpp32s plnpkdind, Rpp32u max_height, Rpp32u max_width);
+RppStatus hip_exec_max_batch(Rpp8u *srcPtr1, Rpp8u *srcPtr2, Rpp8u *dstPtr, rpp::Handle& handle, RppiChnFormat chnFormat, Rpp32u channel, Rpp32s plnpkdind, Rpp32u max_height, Rpp32u max_width);
+
 
 #endif //RPP_HIP_HOST_DECLS_H
