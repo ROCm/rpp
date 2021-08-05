3f4cf82dfb3f10576762afe6ed9bc4aedda9bf93
r-abishek <abishek@multicorewareinc.com>
2021-08-04 21:29:38 -0700

    Add tensor unittest suite support for layout toggle

--- utilities/rpp-unittests/HOST_NEW/Tensor_host_pln1.cpp
+++ utilities/rpp-unittests/HOST_NEW/Tensor_host_pln1.cpp
@@ -72,7 +72,6 @@ int main(int argc, char **argv)
     {
     case 0:
         strcpy(funcName, "brightness");
-        outputFormatToggle = 0;
         break;
     }
 
--- utilities/rpp-unittests/HOST_NEW/Tensor_host_pln3.cpp
+++ utilities/rpp-unittests/HOST_NEW/Tensor_host_pln3.cpp
@@ -66,7 +66,6 @@ int main(int argc, char **argv)
     {
     case 0:
         strcpy(funcName, "brightness");
-        outputFormatToggle = 0;
         break;
     }
 
