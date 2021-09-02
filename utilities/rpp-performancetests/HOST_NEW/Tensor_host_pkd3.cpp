cd81377c6213c461403d305f67fd2361cfcfcdf1
r-abishek <abishek@multicorewareinc.com>
2021-09-01 19:00:12 -0700

    Add omp_time_used

--- utilities/rpp-performancetests/HOST_NEW/Tensor_host_pkd3.cpp
+++ utilities/rpp-performancetests/HOST_NEW/Tensor_host_pkd3.cpp
@@ -416,7 +416,7 @@ int main(int argc, char **argv)
     {
         clock_t start, end;
         double start_omp, end_omp;
-        double cpu_time_used;
+        double cpu_time_used, omp_time_used;
         switch (test_case)
         {
         case 0:
@@ -482,6 +482,7 @@ int main(int argc, char **argv)
         }
 
         cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+        omp_time_used = end_omp - start_omp;
         if (cpu_time_used > max_time_used)
             max_time_used = cpu_time_used;
         if (cpu_time_used < min_time_used)
