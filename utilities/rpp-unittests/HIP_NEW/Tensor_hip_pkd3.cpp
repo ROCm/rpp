edaeeed2b4c497ebac92336d5eb524b0c9b32d75
r-abishek <abishek@multicorewareinc.com>
2021-10-14 13:51:45 -0400

    Add color_cast test suite for hip

--- utilities/rpp-unittests/HIP_NEW/Tensor_hip_pkd3.cpp
+++ utilities/rpp-unittests/HIP_NEW/Tensor_hip_pkd3.cpp
@@ -70,6 +70,9 @@ int main(int argc, char **argv)
     case 2:
         strcpy(funcName, "blend");
         break;
+    case 31:
+        strcpy(funcName, "color_cast");
+        break;
     default:
         strcpy(funcName, "test_case");
         break;
@@ -645,6 +648,63 @@ int main(int argc, char **argv)
             missingFuncFlag = 1;
         else if (ip_bitDepth == 5)
             rppt_blend_gpu(d_inputi8, d_inputi8_second, srcDescPtr, d_outputi8, dstDescPtr, alpha, d_roiTensorPtrSrc, roiTypeSrc, handle);
+        else if (ip_bitDepth == 6)
+            missingFuncFlag = 1;
+        else
+            missingFuncFlag = 1;
+
+        end = clock();
+
+        break;
+    }
+    case 31:
+    {
+        test_case_name = "color_cast";
+
+        Rpp8u rTensor[images];
+        Rpp8u gTensor[images];
+        Rpp8u bTensor[images];
+        Rpp32f alphaTensor[images];
+        for (i = 0; i < images; i++)
+        {
+            rTensor[i] = 0;
+            gTensor[i] = 0;
+            bTensor[i] = 100;
+            alphaTensor[i] = 0.5;
+
+            // xywhROI override sample
+            // roiTensorPtrSrc[i].xywhROI.xy.x = 0;
+            // roiTensorPtrSrc[i].xywhROI.xy.y = 0;
+            // roiTensorPtrSrc[i].xywhROI.roiWidth = 100;
+            // roiTensorPtrSrc[i].xywhROI.roiHeight = 180;
+
+            // ltrbROI override sample
+            // roiTensorPtrSrc[i].ltrbROI.lt.x = 50;
+            // roiTensorPtrSrc[i].ltrbROI.lt.y = 30;
+            // roiTensorPtrSrc[i].ltrbROI.rb.x = 210;
+            // roiTensorPtrSrc[i].ltrbROI.rb.y = 210;
+        }
+
+        // Change RpptRoiType for ltrbROI override sample
+        // roiTypeSrc = RpptRoiType::LTRB;
+        // roiTypeDst = RpptRoiType::LTRB;
+
+        hipMemcpy(d_roiTensorPtrSrc, roiTensorPtrSrc, images * sizeof(RpptROI), hipMemcpyHostToDevice);
+
+        start = clock();
+
+        if (ip_bitDepth == 0)
+            rppt_color_cast_gpu(d_input, srcDescPtr, d_output, dstDescPtr, rTensor, gTensor, bTensor, alphaTensor, d_roiTensorPtrSrc, roiTypeSrc, handle);
+        else if (ip_bitDepth == 1)
+            rppt_color_cast_gpu(d_inputf16, srcDescPtr, d_outputf16, dstDescPtr, rTensor, gTensor, bTensor, alphaTensor, d_roiTensorPtrSrc, roiTypeSrc, handle);
+        else if (ip_bitDepth == 2)
+            rppt_color_cast_gpu(d_inputf32, srcDescPtr, d_outputf32, dstDescPtr, rTensor, gTensor, bTensor, alphaTensor, d_roiTensorPtrSrc, roiTypeSrc, handle);
+        else if (ip_bitDepth == 3)
+            missingFuncFlag = 1;
+        else if (ip_bitDepth == 4)
+            missingFuncFlag = 1;
+        else if (ip_bitDepth == 5)
+            rppt_color_cast_gpu(d_inputi8, srcDescPtr, d_outputi8, dstDescPtr, rTensor, gTensor, bTensor, alphaTensor, d_roiTensorPtrSrc, roiTypeSrc, handle);
         else if (ip_bitDepth == 6)
             missingFuncFlag = 1;
         else
