ea03107396e2a2ce8888a9e39175602b49e2e176
r-abishek <abishek@multicorewareinc.com>
2022-11-22 16:11:30 -0800

    Initial commit - non linear blend host u8

--- include/rppt_tensor_color_augmentations.h
+++ include/rppt_tensor_color_augmentations.h
@@ -71,7 +71,8 @@ RppStatus rppt_gamma_correction_gpu(RppPtr_t srcPtr, RpptDescPtr srcDescPtr, Rpp
 /******************** blend ********************/
 
 // Alpha blending augmentation for a NCHW/NHWC layout tensor
-// *param[in] srcPtr source tensor memory
+// *param[in] srcPtr1 source tensor memory
+// *param[in] srcPtr2 source tensor memory
 // *param[in] srcDescPtr source tensor descriptor
 // *param[out] dstPtr destination tensor memory
 // *param[in] dstDescPtr destination tensor descriptor
