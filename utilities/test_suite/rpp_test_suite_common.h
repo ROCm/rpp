171b102c5fc14348195c12ea6337c2da802317a9
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2023-01-12 21:48:12 -0800

    added a Map for finding augmentation name from case number

--- utilities/test_suite/rpp_test_suite_common.h
+++ utilities/test_suite/rpp_test_suite_common.h
@@ -13,209 +13,39 @@
 #include <omp.h>
 #include <half/half.hpp>
 #include <fstream>
-// #include "HOST_NEW/helpers/testSuite_helper.hpp"
-// #include "HIP_NEW/helpers/testSuite_helper.hpp"
 #include <experimental/filesystem>
 
 using namespace cv;
 using namespace std;
 
-#ifndef TESTSUITE_HELPER
-#define TESTSUITE_HELPER
-
-#include "rppi.h"
-
-RppStatus compute_image_location_host(RppiSize batch_srcSizeMax, int batchCount, Rpp32u *loc, Rpp32u channel)
+std::map<int, string> augmentationMap =
 {
-    for (int m = 0; m < batchCount; m++)
-    {
-        *loc += (batch_srcSizeMax.height * batch_srcSizeMax.width);
-    }
-    *loc *= channel;
-
-    return RPP_SUCCESS;
-}
-
-template <typename T>
-inline RppStatus compute_unpadded_from_padded_host(T* srcPtrPadded, RppiSize srcSize, RppiSize srcSizeMax, T* dstPtrUnpadded,
-                                                   RppiChnFormat chnFormat, Rpp32u channel)
-{
-    T *srcPtrPaddedChannel, *srcPtrPaddedRow, *dstPtrUnpaddedRow;
-    Rpp32u imageDimMax = srcSizeMax.height * srcSizeMax.width;
-    dstPtrUnpaddedRow = dstPtrUnpadded;
-
-    if (chnFormat == RPPI_CHN_PLANAR)
-    {
-        for (int c = 0; c < channel; c++)
-        {
-            srcPtrPaddedChannel = srcPtrPadded + (c * imageDimMax);
-            for (int i = 0; i < srcSize.height; i++)
-            {
-                srcPtrPaddedRow = srcPtrPaddedChannel + (i * srcSizeMax.width);
-                memcpy(dstPtrUnpaddedRow, srcPtrPaddedRow, srcSize.width * sizeof(T));
-                dstPtrUnpaddedRow += srcSize.width;
-            }
-        }
-    }
-    else if (chnFormat == RPPI_CHN_PACKED)
-    {
-        Rpp32u elementsInRowMax = channel * srcSizeMax.width;
-        Rpp32u elementsInRow = channel * srcSize.width;
-        for (int i = 0; i < srcSize.height; i++)
-        {
-            srcPtrPaddedRow = srcPtrPadded + (i * elementsInRowMax);
-            memcpy(dstPtrUnpaddedRow, srcPtrPaddedRow, elementsInRow * sizeof(T));
-            dstPtrUnpaddedRow += elementsInRow;
-        }
-    }
-
-    return RPP_SUCCESS;
-}
-
-template <typename T>
-inline RppStatus compute_padded_from_unpadded_host(T* srcPtrUnpadded, RppiSize srcSize, RppiSize dstSizeMax, T* dstPtrPadded,
-                                                   RppiChnFormat chnFormat, Rpp32u channel)
-{
-    T *dstPtrPaddedChannel, *dstPtrPaddedRow, *srcPtrUnpaddedRow;
-    Rpp32u imageDimMax = dstSizeMax.height * dstSizeMax.width;
-    srcPtrUnpaddedRow = srcPtrUnpadded;
-
-    if (chnFormat == RPPI_CHN_PLANAR)
-    {
-        for (int c = 0; c < channel; c++)
-        {
-            dstPtrPaddedChannel = dstPtrPadded + (c * imageDimMax);
-            for (int i = 0; i < srcSize.height; i++)
-            {
-                dstPtrPaddedRow = dstPtrPaddedChannel + (i * dstSizeMax.width);
-                memcpy(dstPtrPaddedRow, srcPtrUnpaddedRow, srcSize.width * sizeof(T));
-                srcPtrUnpaddedRow += srcSize.width;
-            }
-        }
-    }
-    else if (chnFormat == RPPI_CHN_PACKED)
-    {
-        Rpp32u elementsInRowMax = channel * dstSizeMax.width;
-        Rpp32u elementsInRow = channel * srcSize.width;
-        for (int i = 0; i < srcSize.height; i++)
-        {
-            dstPtrPaddedRow = dstPtrPadded + (i * elementsInRowMax);
-            memcpy(dstPtrPaddedRow, srcPtrUnpaddedRow, elementsInRow * sizeof(T));
-            srcPtrUnpaddedRow += elementsInRow;
-        }
-    }
-
-    return RPP_SUCCESS;
-}
-
-template <typename T>
-inline RppStatus generate_bressenham_line_host(T *dstPtr, RppiSize dstSize, Rpp32u *endpoints, Rpp32u *rasterCoordinates)
-{
-    Rpp32u *rasterCoordinatesTemp;
-    rasterCoordinatesTemp = rasterCoordinates;
-
-    Rpp32s x0 = *endpoints;
-    Rpp32s y0 = *(endpoints + 1);
-    Rpp32s x1 = *(endpoints + 2);
-    Rpp32s y1 = *(endpoints + 3);
-
-    Rpp32s dx, dy;
-    Rpp32s stepX, stepY;
-
-    dx = x1 - x0;
-    dy = y1 - y0;
-
-    if (dy < 0)
-    {
-        dy = -dy;
-        stepY = -1;
-    }
-    else
-    {
-        stepY = 1;
-    }
-
-    if (dx < 0)
-    {
-        dx = -dx;
-        stepX = -1;
-    }
-    else
-    {
-        stepX = 1;
-    }
-
-    dy <<= 1;
-    dx <<= 1;
-
-    if ((0 <= x0) && (x0 < dstSize.width) && (0 <= y0) && (y0 < dstSize.height))
-    {
-        *(dstPtr + (y0 * dstSize.width) + x0) = (T) 255;
-        *rasterCoordinatesTemp = y0;
-        rasterCoordinatesTemp++;
-        *rasterCoordinatesTemp = x0;
-        rasterCoordinatesTemp++;
-    }
-
-    if (dx > dy)
-    {
-        Rpp32s fraction = dy - (dx >> 1);
-        while (x0 != x1)
-        {
-            x0 += stepX;
-            if (fraction >= 0)
-            {
-                y0 += stepY;
-                fraction -= dx;
-            }
-            fraction += dy;
-            if ((0 <= x0) && (x0 < dstSize.width) && (0 <= y0) && (y0 < dstSize.height))
-            {
-                *(dstPtr + (y0 * dstSize.width) + x0) = (T) 255;
-                *rasterCoordinatesTemp = y0;
-                rasterCoordinatesTemp++;
-                *rasterCoordinatesTemp = x0;
-                rasterCoordinatesTemp++;
-            }
-        }
-    }
-    else
-    {
-        int fraction = dx - (dy >> 1);
-        while (y0 != y1)
-        {
-            if (fraction >= 0)
-            {
-                x0 += stepX;
-                fraction -= dy;
-            }
-            y0 += stepY;
-            fraction += dx;
-            if ((0 <= x0) && (x0 < dstSize.width) && (0 <= y0) && (y0 < dstSize.height))
-            {
-                *(dstPtr + (y0 * dstSize.width) + x0) = (T) 255;
-                *rasterCoordinatesTemp = y0;
-                rasterCoordinatesTemp++;
-                *rasterCoordinatesTemp = x0;
-                rasterCoordinatesTemp++;
-            }
-        }
-    }
-
-    return RPP_SUCCESS;
-}
-
-#endif
-
-void remove_substring(string &str, string &pattern)
-{
-    std::string::size_type i = str.find(pattern);
-    while (i != std::string::npos)
-    {
-        str.erase(i, pattern.length());
-        i = str.find(pattern, i);
-   }
-}
+    {0, "brightness"},
+    {1, "gamma_correction"},
+    {2, "blend"},
+    {4, "contrast"},
+    {8, "noise"},
+    {13, "exposure"},
+    {20, "flip"},
+    {21, "resize"},
+    {23, "rotate"},
+    {24, "warp_affine"},
+    {30, "non_linear_blend"},
+    {31, "color_cast"},
+    {36, "color_twist"},
+    {37, "crop"},
+    {38, "resize_crop_mirror"},
+    {39, "gamma_correction"},
+    {40, "erode"},
+    {41, "dilate"},
+    {49, "box_filter"},
+    {70, "copy"},
+    {80, "resize_mirror_normalize"},
+    {83, "grid_mask"},
+    {84, "spatter"},
+    {85, "swap_channels"},
+    {86, "color_to_greyscale"}
+};
 
 std::string get_interpolation_type(unsigned int val, RpptInterpolationType &interpolationType)
 {
@@ -408,6 +238,16 @@ void convert_pkd3_to_pln3(Rpp8u *input, RpptDescPtr srcDescPtr)
     }
 
     free(inputCopy);
+}
+
+void remove_substring(string &str, string &pattern)
+{
+    std::string::size_type i = str.find(pattern);
+    while (i != std::string::npos)
+    {
+        str.erase(i, pattern.length());
+        i = str.find(pattern, i);
+   }
 }
 
 template <typename T>
