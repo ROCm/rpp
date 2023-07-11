20599403941f8357f96112d604ee4c185182b29c
Snehaa Giridharan <snehaa@multicorewareinc.com>
2023-07-11 11:29:44 -0400

    Fix NDHWC layout handling in testsuite
    Temporarily converts pln3 inputs into pkd3 inputs
    later stores them as pln3 after processing

--- utilities/test_suite/HOST/Tensor_voxel_host.cpp
+++ utilities/test_suite/HOST/Tensor_voxel_host.cpp
@@ -321,6 +321,12 @@ int main(int argc, char * argv[])
     // set src/dst generic tensor descriptors
     RpptGenericDesc descriptor3D;
     RpptGenericDescPtr descriptorPtr3D = &descriptor3D;
+    bool flag_pkd3_processing = 0;
+    if (layoutType == 1)
+    {
+        layoutType = 0;           //Temporary change as our inputs are of type pln3
+        flag_pkd3_processing = 1;
+    }
     set_generic_descriptor(descriptorPtr3D, batchSize, maxX, maxY, maxZ, numChannels, offsetInBytes, layoutType);
 
     // set src/dst xyzwhd ROI tensors
@@ -329,12 +335,12 @@ int main(int argc, char * argv[])
 
     // optionally pick full image as ROI or a smaller slice of the 3D tensor in X/Y/Z dimensions
     // option 1 - test using roi as the whole 3D image - not sliced (example for 240 x 240 x 155 x 1)
-    // roiGenericSrcPtr[0].xyzwhdROI.xyz.x = 0;                              // start X dim = 0
-    // roiGenericSrcPtr[0].xyzwhdROI.xyz.y = 0;                              // start Y dim = 0
-    // roiGenericSrcPtr[0].xyzwhdROI.xyz.z = 0;                              // start Z dim = 0
-    // roiGenericSrcPtr[0].xyzwhdROI.roiWidth = niftiHeader.dim[1];          // length in X dim = 240
-    // roiGenericSrcPtr[0].xyzwhdROI.roiHeight = niftiHeader.dim[2];         // length in Y dim = 240
-    // roiGenericSrcPtr[0].xyzwhdROI.roiDepth = niftiHeader.dim[3];          // length in Z dim = 155
+    roiGenericSrcPtr[0].xyzwhdROI.xyz.x = 0;                              // start X dim = 0
+    roiGenericSrcPtr[0].xyzwhdROI.xyz.y = 0;                              // start Y dim = 0
+    roiGenericSrcPtr[0].xyzwhdROI.xyz.z = 0;                              // start Z dim = 0
+    roiGenericSrcPtr[0].xyzwhdROI.roiWidth = niftiHeader.dim[1];          // length in X dim = 240
+    roiGenericSrcPtr[0].xyzwhdROI.roiHeight = niftiHeader.dim[2];         // length in Y dim = 240
+    roiGenericSrcPtr[0].xyzwhdROI.roiDepth = niftiHeader.dim[3];          // length in Z dim = 155
     // option 2 - test using roi as a smaller 3D tensor slice - sliced in X, Y and Z dims (example for 240 x 240 x 155 x 1)
     // roiGenericSrcPtr[0].xyzwhdROI.xyz.x = niftiHeader.dim[1] / 4;         // start X dim = 60
     // roiGenericSrcPtr[0].xyzwhdROI.xyz.y = niftiHeader.dim[2] / 4;         // start Y dim = 60
@@ -350,12 +356,12 @@ int main(int argc, char * argv[])
     // roiGenericSrcPtr[0].xyzwhdROI.roiHeight = niftiHeader.dim[2];         // length in Y dim = 240
     // roiGenericSrcPtr[0].xyzwhdROI.roiDepth = niftiHeader.dim[3] / 3;      // length in Z dim = 51
     // option 4 - test using roi as a smaller 3D tensor slice - sliced in only X and Z dim (example for 240 x 240 x 155 x 1)
-    roiGenericSrcPtr[0].xyzwhdROI.xyz.x = niftiHeader.dim[1] / 5;         // start X dim = 48
-    roiGenericSrcPtr[0].xyzwhdROI.xyz.y = 0;                              // start Y dim = 0
-    roiGenericSrcPtr[0].xyzwhdROI.xyz.z = niftiHeader.dim[3] / 3;         // start Z dim = 51
-    roiGenericSrcPtr[0].xyzwhdROI.roiWidth = niftiHeader.dim[1] * 3 / 5;  // length in X dim = 144
-    roiGenericSrcPtr[0].xyzwhdROI.roiHeight = niftiHeader.dim[2];         // length in Y dim = 240
-    roiGenericSrcPtr[0].xyzwhdROI.roiDepth = niftiHeader.dim[3] / 3;      // length in Z dim = 51
+    // roiGenericSrcPtr[0].xyzwhdROI.xyz.x = niftiHeader.dim[1] / 5;         // start X dim = 48
+    // roiGenericSrcPtr[0].xyzwhdROI.xyz.y = 0;                              // start Y dim = 0
+    // roiGenericSrcPtr[0].xyzwhdROI.xyz.z = niftiHeader.dim[3] / 3;         // start Z dim = 51
+    // roiGenericSrcPtr[0].xyzwhdROI.roiWidth = niftiHeader.dim[1] * 3 / 5;  // length in X dim = 144
+    // roiGenericSrcPtr[0].xyzwhdROI.roiHeight = niftiHeader.dim[2];         // length in Y dim = 240
+    // roiGenericSrcPtr[0].xyzwhdROI.roiDepth = niftiHeader.dim[3] / 3;      // length in Z dim = 51
 
     // Set buffer sizes in pixels for src/dst
     Rpp64u iBufferSize = (Rpp64u)descriptorPtr3D->strides[0] * (Rpp64u)descriptorPtr3D->dims[0] * numChannels;
@@ -374,6 +380,11 @@ int main(int argc, char * argv[])
     for(int i = 0; i < numChannels; i++) //temporary change to process pln3
     {
         convert_input_niftitype_to_float_generic(niftiData, &niftiHeader, inputF32 + i * increment, descriptorPtr3D);
+    }
+    if (flag_pkd3_processing)
+    {
+        convert_pln3_to_pkd3(inputF32, descriptorPtr3D);
+        set_generic_descriptor(descriptorPtr3D, batchSize, maxX, maxY, maxZ, numChannels, offsetInBytes, 1);
     }
 
     // set argument tensors
@@ -455,6 +466,11 @@ int main(int argc, char * argv[])
 
     if(testType == 0)
     {
+        if (flag_pkd3_processing)
+        {
+            convert_pkd3_to_pln3(outputF32, descriptorPtr3D);
+            set_generic_descriptor(descriptorPtr3D, batchSize, maxX, maxY, maxZ, numChannels, offsetInBytes, 0);
+        }
         for(int i = 0; i < numChannels; i++) // temporary changes to process pln3
         {
             // Convert RpptDataType::F32 strided buffer to default NIFTI_DATATYPE unstrided buffer
@@ -486,8 +502,25 @@ int main(int argc, char * argv[])
 
             // write nifti file
             write_nifti_file(&niftiHeader, niftiData);
-            std::string command = "convert -delay 10 -loop 0 $(ls -v | grep jpg) niftiOutput_chn" + std::to_string(i) + ".gif";
-            system(command.c_str());
+
+            // std::string command = "convert -delay 10 -loop 0 $(ls -v | grep jpg | grep '_$i_') niftiOutput_chn" + std::to_string(i) + ".gif";
+            // system(command.c_str());
+
+            if(i == 0)
+            {
+                std::string command = "convert -delay 10 -loop 0 $(ls -v | grep jpg | grep chn_0_) niftiOutput_chn" + std::to_string(i) + ".gif";
+                system(command.c_str());
+            }
+            if(i == 1)
+            {
+                std::string command = "convert -delay 10 -loop 0 $(ls -v | grep jpg | grep chn_1_) niftiOutput_chn" + std::to_string(i) + ".gif";
+                system(command.c_str());
+            }
+            if(i == 2)
+            {
+                std::string command = "convert -delay 10 -loop 0 $(ls -v | grep jpg | grep chn_2_) niftiOutput_chn" + std::to_string(i) + ".gif";
+                system(command.c_str());
+            }
             free(outputBufferOpenCV);
         }
     }
