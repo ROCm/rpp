efcab0ddbc03c698bdc4dd3d0d58feec91b2743f
snehaa8 <snehaa@multicorewareinc.com>
2023-11-20 12:22:24 -0500

    fix qa mismatches

--- utilities/test_suite/rpp_test_suite_audio.h
+++ utilities/test_suite/rpp_test_suite_audio.h
@@ -39,6 +39,7 @@ std::map<int, string> audioAugmentationMap =
     {2, "pre_emphasis_filter"},
     {3, "down_mixing"},
     {4, "slice_audio"},
+    {5, "mel_filter_bank"}
 };
 
 // Golden outputs for Non Silent Region Detection
@@ -126,6 +127,61 @@ void read_audio_batch_and_fill_dims(RpptDescPtr descPtr, Rpp32f *inputf32, vecto
 
         // Close input
         sf_close (infile);
+    }
+}
+
+void read_from_text_files(Rpp32f *srcPtr, RpptDescPtr srcDescPtr, RpptImagePatch *srcDims, string test_case, int read_type, vector<std::string> audioNames)
+{
+    fstream ref_file;
+    string ref_path = get_current_dir_name();
+    string pattern = "HOST/build";
+    remove_substring(ref_path, pattern);
+    ref_path = ref_path + "REFERENCE_OUTPUTS_AUDIO/";
+
+    string read_type_str;
+    if(read_type == 0)
+        read_type_str = "_ref_";
+    else
+        read_type_str = "_info_";
+
+    for (int batchcount = 0; batchcount < srcDescPtr->n; batchcount++)
+    {
+        string current_file_name = audioNames[batchcount];
+        size_t last_index = current_file_name.find_last_of(".");
+        current_file_name = current_file_name.substr(0, last_index);  // Remove extension from file name
+        string out_file = ref_path + test_case + "/" + test_case + read_type_str + current_file_name + ".txt";
+        ref_file.open(out_file, ios::in);
+        if(!ref_file.is_open())
+        {
+            cerr<<"Unable to open the file specified! Please check the path of the file given as input"<<endl;
+            break;
+        }
+
+        if(read_type == 0)
+        {
+            Rpp32f ref_val;
+            Rpp32f *srcPtrCurrent = srcPtr + batchcount * srcDescPtr->strides.nStride;
+            Rpp32f *srcPtrRow = srcPtrCurrent;
+            for(int i = 0; i < srcDims[batchcount].height; i++)
+            {
+                Rpp32f *srcPtrTemp = srcPtrRow;
+                for(int j = 0; j < srcDims[batchcount].width; j++)
+                {
+                    ref_file>>ref_val;
+                    srcPtrTemp[j] = ref_val;
+                }
+                srcPtrRow += srcDescPtr->strides.hStride;
+            }
+        }
+        else
+        {
+            Rpp32s ref_height, ref_width;
+            ref_file>>ref_height;
+            ref_file>>ref_width;
+            srcDims[batchcount].height = ref_height;
+            srcDims[batchcount].width = ref_width;
+        }
+        ref_file.close();
     }
 }
 
