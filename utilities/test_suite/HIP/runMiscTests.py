a7df2eb35b0c25ed4f91ba69a8cea3e195fb204f
r-abishek <abishek@multicorewareinc.com>
2023-11-30 00:52:15 -0500

    Add perf test with profiler option on misc hip tests

--- utilities/test_suite/HIP/runMiscTests.py
+++ utilities/test_suite/HIP/runMiscTests.py
@@ -79,6 +79,33 @@ def get_log_file_list():
         "../../OUTPUT_PERFORMANCE_MISC_LOGS_HIP_" + timestamp + "/Tensor_misc_hip_raw_performance_log.txt",
     ]
 
+def case_file_check(CASE_FILE_PATH, new_file):
+    try:
+        case_file = open(CASE_FILE_PATH,'r')
+        for line in case_file:
+            print(line)
+            if not(line.startswith('"Name"')):
+                new_file.write(line)
+        case_file.close()
+        return True
+    except IOError:
+        print("Unable to open case results")
+        return False
+
+# Generate performance reports based on counters and a list of types
+def generate_performance_reports(RESULTS_DIR):
+    import pandas as pd
+    pd.options.display.max_rows = None
+    # Generate performance report
+    df = pd.read_csv(RESULTS_DIR + "/consolidated_results.stats.csv")
+    df["AverageMs"] = df["AverageNs"] / 1000000
+    dfPrint = df.drop(['Percentage'], axis = 1)
+    dfPrint["HIP Kernel Name"] = dfPrint.iloc[:,0].str.lstrip("Hip_")
+    dfPrint_noIndices = dfPrint.astype(str)
+    dfPrint_noIndices.replace(['0', '0.0'], '', inplace = True)
+    dfPrint_noIndices = dfPrint_noIndices.to_string(index = False)
+    print(dfPrint_noIndices)
+
 def run_unit_test(numDims, case, numRuns, testType, bitDepth, batchSize, outFilePath):
     print("\n\n\n\n")
     print("--------------------------------")
@@ -106,6 +133,27 @@ def run_performance_test(loggingFolder, numDims, case, numRuns, testType, bitDep
             log_file.write(output)
     print("------------------------------------------------------------------------------------------")
 
+def run_performance_test_with_profiler(loggingFolder, numDims, case, numRuns, testType, bitDepth, batchSize, outFilePath):
+    print("\n\n\n\n")
+    print("--------------------------------")
+    print("Running a New Functionality...")
+    print("--------------------------------")
+    print(loggingFolder)
+
+    if not os.path.exists(f"{outFilePath}/case_{case}"):
+        os.mkdir(f"{outFilePath}/case_{case}")
+
+    with open("{}/Tensor_misc_hip_raw_performance_log.txt".format(loggingFolder), "a") as log_file:
+        print(f"\nrocprof --basenames on --timestamp on --stats -o {outFilePath}/case_{case}/output_case{case}.csv ./Tensor_misc_hip {case} {testType} {numDims} {batchSize} {numRuns}")
+        process = subprocess.Popen([ 'rocprof', '--basenames', 'on', '--timestamp', 'on', '--stats', '-o', f"{outFilePath}/case_{case}/output_case{case}.csv", "./Tensor_misc_hip", str(case), str(testType), str(numDims), str(batchSize), str(numRuns) , outFilePath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)    # nosec
+        while True:
+            output = process.stdout.readline()
+            if not output and process.poll() is not None:
+                break
+            print(output.strip())
+            log_file.write(output)
+    print("------------------------------------------------------------------------------------------")
+
 # Parse and validate command-line arguments for the RPP test suite
 def rpp_test_suite_parser_and_validator():
     parser = argparse.ArgumentParser()
@@ -115,6 +163,7 @@ def rpp_test_suite_parser_and_validator():
     parser.add_argument('--case_list', nargs = "+", help = "List of case numbers to test", required = False)
     parser.add_argument("--num_dims", type = int, default = 2, help = "Number of dimensions for input")
     parser.add_argument('--num_runs', type = int, default = 1, help = "Specifies the number of runs for running the performance tests")
+    parser.add_argument('--profiling', type = str , default = 'NO', help = 'Run with profiler? - (YES/NO)', required = False)
     parser.add_argument('--batch_size', type = int, default = 1, help = "Specifies the batch size to use for running tests. Default is 1.")
     parser.add_argument('--preserve_output', type = int, default = 1, help = "preserves the output of the program - (0 = override output / 1 = preserve output )" )
     args = parser.parse_args()
@@ -141,6 +190,8 @@ def rpp_test_suite_parser_and_validator():
     elif args.test_type == 0 and args.num_dims != 2 and args.num_dims != 3 and args.num_dims != 4:
         print("Inavlid Input! QA mode is supported only for num_dims = 2 / 3 / 4!")
         exit(0)
+    elif args.profiling != 'YES' and args.profiling != 'NO':
+        print("Profiling option value must be either 'YES' or 'NO'.")
 
     if args.case_list is None:
         args.case_list = range(args.case_start, args.case_end + 1)
@@ -159,6 +210,7 @@ testType = args.test_type
 caseList = args.case_list
 numDims = args.num_dims
 numRuns = args.num_runs
+profilingOption = args.profiling
 batchSize = args.batch_size
 preserveOutput = args.preserve_output
 bitDepth = 2 # Current audio test suite only supports bit depth 2
@@ -203,13 +255,62 @@ if testType == 0:
             continue
 
         run_unit_test(numDims, case, numRuns, testType, bitDepth, batchSize, outFilePath)
-else:
+
+elif (testType == 1 and profilingOption == "NO"):
     for case in caseList:
         if int(case) != 0:
             print(f"Invalid case number {case}. Case number must be 0!")
             continue
 
         run_performance_test(loggingFolder, numDims, case, numRuns, testType, bitDepth, batchSize, outFilePath)
+
+elif (testType == 1 and profilingOption == "YES"):
+    for case in caseList:
+        if int(case) != 0:
+            print(f"Invalid case number {case}. Case number must be 0!")
+            continue
+
+        run_performance_test_with_profiler(loggingFolder, numDims, case, numRuns, testType, bitDepth, batchSize, outFilePath)
+
+    RESULTS_DIR = ""
+    RESULTS_DIR = "../../OUTPUT_PERFORMANCE_MISC_LOGS_HIP_" + timestamp
+    print("RESULTS_DIR = " + RESULTS_DIR)
+    CONSOLIDATED_FILE = RESULTS_DIR + "/consolidated_results.stats.csv"
+
+    CASE_NUM_LIST = caseList
+    BIT_DEPTH_LIST = [2]
+    OFT_LIST = [0]
+
+    # Open csv file
+    new_file = open(CONSOLIDATED_FILE, 'w')
+    new_file.write('"HIP Kernel Name","Calls","TotalDurationNs","AverageNs","Percentage"\n')
+
+    # Loop through cases
+    for CASE_NUM in CASE_NUM_LIST:
+        # Set results directory
+        CASE_RESULTS_DIR = RESULTS_DIR + "/case_" + str(CASE_NUM)
+        print("CASE_RESULTS_DIR = " + CASE_RESULTS_DIR)
+
+        # Loop through bit depths
+        for BIT_DEPTH in BIT_DEPTH_LIST:
+            # Loop through output format toggle cases
+            for OFT in OFT_LIST:
+                # Write into csv file
+                CASE_FILE_PATH = CASE_RESULTS_DIR + "/output_case" + str(CASE_NUM) + ".stats.csv"
+                print("CASE_FILE_PATH = " + CASE_FILE_PATH)
+                fileCheck = case_file_check(CASE_FILE_PATH, new_file)
+                if fileCheck == False:
+                    continue
+
+    new_file.close()
+    subprocess.call(['chown', '{}:{}'.format(os.getuid(), os.getgid()), CONSOLIDATED_FILE])  # nosec
+    try:
+        generate_performance_reports(RESULTS_DIR)
+    except ImportError:
+        print("\nPandas not available! Results of GPU profiling experiment are available in the following files:\n" + \
+                CONSOLIDATED_FILE + "\n")
+    except IOError:
+        print("Unable to open results in " + CONSOLIDATED_FILE)
 
 # print the results of qa tests
 supportedCaseList = ['0']
@@ -229,7 +330,7 @@ if testType == 0:
 print("\n-------------- " + caseInfo + " --------------")
 
 # Performance tests
-if (testType == 1):
+if (testType == 1 and profilingOption == "NO"):
     log_file_list = get_log_file_list()
 
     try:
