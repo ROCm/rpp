5dda0f049387e092026f254718ff024d0dddc4ad
r-abishek <abishek@multicorewareinc.com>
2023-12-05 11:58:24 -0500

    Add fix for warnings on hip build

--- src/include/hip/rpp_hip_common.hpp
+++ src/include/hip/rpp_hip_common.hpp
@@ -126,7 +126,7 @@ struct RPPTensorFunctionMetaData
 #define ONE_OVER_256                    0.00390625f
 #define SIX_OVER_360                    0.01666667f
 #define RPP_255_OVER_1PT57              162.3380757272f     // (255 / 1.570796) - multiplier used in phase computation
-#define ONE_OVER_1PT57                  0.6366199048F       // (1 / 1.570796) i.e. 2/pi - multiplier used in phase computation
+#define ONE_OVER_1PT57                  0.6366199048f       // (1 / 1.570796) i.e. 2/pi - multiplier used in phase computation
 #define PI                              3.14159265
 #define RGB_TO_GREY_WEIGHT_RED          0.299f
 #define RGB_TO_GREY_WEIGHT_GREEN        0.587f
