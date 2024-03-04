bd9abed1929540b301bc87b958081732af5d23a1
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2024-03-04 08:20:03 +0000

    updated the slice voxel testing configuration to validate the kernel correctly

--- utilities/test_suite/HIP/Tensor_voxel_hip.cpp
+++ utilities/test_suite/HIP/Tensor_voxel_hip.cpp
@@ -274,13 +274,17 @@ int main(int argc, char * argv[])
                             int idx1 = i * 4;
                             int idx2 = i * 8;
                             roiTensor[idx2] = anchorTensor[idx1] = 0;
-                            roiTensor[idx2 + 1] = anchorTensor[idx1 + 1] = 0;
-                            roiTensor[idx2 + 2] = anchorTensor[idx1 + 2] = 0;
-                            roiTensor[idx2 + 3] = anchorTensor[idx1 + 3] = 0;
-                            roiTensor[idx2 + 4] = shapeTensor[idx1] = descriptorPtr3D->dims[1];
-                            roiTensor[idx2 + 5] = shapeTensor[idx1 + 1] = roiGenericSrcPtr[i].xyzwhdROI.roiDepth;
-                            roiTensor[idx2 + 6] = shapeTensor[idx1 + 2] = roiGenericSrcPtr[i].xyzwhdROI.roiHeight;
-                            roiTensor[idx2 + 7] = shapeTensor[idx1 + 3] = roiGenericSrcPtr[i].xyzwhdROI.roiWidth;
+                            roiTensor[idx2 + 1] = anchorTensor[idx1 + 1] = roiGenericSrcPtr[i].xyzwhdROI.xyz.z;
+                            roiTensor[idx2 + 2] = anchorTensor[idx1 + 2] = roiGenericSrcPtr[i].xyzwhdROI.xyz.y;
+                            roiTensor[idx2 + 3] = anchorTensor[idx1 + 3] = roiGenericSrcPtr[i].xyzwhdROI.xyz.x;
+                            roiTensor[idx2 + 4] = descriptorPtr3D->dims[1];
+                            roiTensor[idx2 + 5] = roiGenericSrcPtr[i].xyzwhdROI.roiDepth;
+                            roiTensor[idx2 + 6] = roiGenericSrcPtr[i].xyzwhdROI.roiHeight;
+                            roiTensor[idx2 + 7] = roiGenericSrcPtr[i].xyzwhdROI.roiWidth;
+                            shapeTensor[idx1] = roiTensor[idx2 + 4];
+                            shapeTensor[idx1 + 1] = roiTensor[idx2 + 5] / 2;
+                            shapeTensor[idx1 + 2] = roiTensor[idx2 + 6] / 2;
+                            shapeTensor[idx1 + 3] = roiTensor[idx2 + 7] / 2;
                         }
                     }
                     else if(descriptorPtr3D->layout == RpptLayout::NDHWC)
@@ -289,14 +293,18 @@ int main(int argc, char * argv[])
                         {
                             int idx1 = i * 4;
                             int idx2 = i * 8;
-                            roiTensor[idx2] = anchorTensor[idx1] = 0;
-                            roiTensor[idx2 + 1] = anchorTensor[idx1 + 1] = 0;
-                            roiTensor[idx2 + 2] = anchorTensor[idx1 + 2] = 0;
+                            roiTensor[idx2] = anchorTensor[idx1] = roiGenericSrcPtr[i].xyzwhdROI.xyz.z;
+                            roiTensor[idx2 + 1] = anchorTensor[idx1 + 1] = roiGenericSrcPtr[i].xyzwhdROI.xyz.y;
+                            roiTensor[idx2 + 2] = anchorTensor[idx1 + 2] = roiGenericSrcPtr[i].xyzwhdROI.xyz.x;
                             roiTensor[idx2 + 3] = anchorTensor[idx1 + 3] = 0;
-                            roiTensor[idx2 + 4] = shapeTensor[idx1] = roiGenericSrcPtr[i].xyzwhdROI.roiDepth;
-                            roiTensor[idx2 + 5] = shapeTensor[idx1 + 1] = roiGenericSrcPtr[i].xyzwhdROI.roiHeight;
-                            roiTensor[idx2 + 6] = shapeTensor[idx1 + 2] = roiGenericSrcPtr[i].xyzwhdROI.roiWidth;
-                            roiTensor[idx2 + 7] = shapeTensor[idx1 + 3] = descriptorPtr3D->dims[4];
+                            roiTensor[idx2 + 4] = roiGenericSrcPtr[i].xyzwhdROI.roiDepth;
+                            roiTensor[idx2 + 5] = roiGenericSrcPtr[i].xyzwhdROI.roiHeight;
+                            roiTensor[idx2 + 6] = roiGenericSrcPtr[i].xyzwhdROI.roiWidth;
+                            roiTensor[idx2 + 7] = descriptorPtr3D->dims[4];
+                            shapeTensor[idx1] = roiTensor[idx2 + 4] / 2;
+                            shapeTensor[idx1 + 1] = roiTensor[idx2 + 5] / 2;
+                            shapeTensor[idx1 + 2] = roiTensor[idx2 + 6] / 2;
+                            shapeTensor[idx1 + 3] = roiTensor[idx2 + 7];
                         }
                     }
 
@@ -406,6 +414,31 @@ int main(int argc, char * argv[])
                     // Copy U8 buffer to F32 buffer for display purposes
                     for(int i = 0; i < bufferLength; i++)
                         outputF32[i] = static_cast<float>(outputU8[i]);
+                }
+
+                if(qaFlag && testCase == 1)
+                {
+                    // update the roi for comparision with the shapeTensor values
+                    if (descriptorPtr3D->layout == RpptLayout::NCDHW)
+                    {
+                        for(int i = 0; i < batchSize; i++)
+                        {
+                            int idx1 = i * 4;
+                            roiGenericSrcPtr[i].xyzwhdROI.roiDepth = shapeTensor[idx1 + 1];
+                            roiGenericSrcPtr[i].xyzwhdROI.roiHeight =  shapeTensor[idx1 + 2];
+                            roiGenericSrcPtr[i].xyzwhdROI.roiWidth = shapeTensor[idx1 + 3];
+                        }
+                    }
+                    else if(descriptorPtr3D->layout == RpptLayout::NDHWC)
+                    {
+                        for(int i = 0; i < batchSize; i++)
+                        {
+                            int idx1 = i * 4;
+                            roiGenericSrcPtr[i].xyzwhdROI.roiDepth = shapeTensor[idx1];
+                            roiGenericSrcPtr[i].xyzwhdROI.roiHeight =  shapeTensor[idx1 + 1];
+                            roiGenericSrcPtr[i].xyzwhdROI.roiWidth = shapeTensor[idx1 + 2];
+                        }
+                    }
                 }
 
                 /*Compare the output of the function with golden outputs only if
