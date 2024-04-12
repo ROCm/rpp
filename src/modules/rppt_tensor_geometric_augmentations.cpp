f1a10a78fc308fab9c658ec8f884f13610ec591e
r-abishek <abishek@multicorewareinc.com>
2024-04-12 18:18:12 -0400

    Remove automerge redundancy

--- src/modules/rppt_tensor_geometric_augmentations.cpp
+++ src/modules/rppt_tensor_geometric_augmentations.cpp
@@ -1160,63 +1160,6 @@ RppStatus rppt_flip_voxel_host(RppPtr_t srcPtr,
     return RPP_SUCCESS;
 }
 
-/******************** flip_voxel ********************/
-
-RppStatus rppt_flip_voxel_host(RppPtr_t srcPtr,
-                               RpptGenericDescPtr srcGenericDescPtr,
-                               RppPtr_t dstPtr,
-                               RpptGenericDescPtr dstGenericDescPtr,
-                               Rpp32u *horizontalTensor,
-                               Rpp32u *verticalTensor,
-                               Rpp32u *depthTensor,
-                               RpptROI3DPtr roiGenericPtrSrc,
-                               RpptRoi3DType roiType,
-                               rppHandle_t rppHandle)
-{
-    RppLayoutParams layoutParams;
-    if ((srcGenericDescPtr->layout == RpptLayout::NCDHW) && (dstGenericDescPtr->layout == RpptLayout::NCDHW))
-        layoutParams = get_layout_params(srcGenericDescPtr->layout, srcGenericDescPtr->dims[1]);
-    else if ((srcGenericDescPtr->layout == RpptLayout::NDHWC) && (dstGenericDescPtr->layout == RpptLayout::NDHWC))
-        layoutParams = get_layout_params(srcGenericDescPtr->layout, srcGenericDescPtr->dims[4]);
-
-    if ((srcGenericDescPtr->dataType != RpptDataType::F32) && (srcGenericDescPtr->dataType != RpptDataType::U8)) return RPP_ERROR_INVALID_SRC_DATATYPE;
-    if ((dstGenericDescPtr->dataType != RpptDataType::F32) && (dstGenericDescPtr->dataType != RpptDataType::U8)) return RPP_ERROR_INVALID_DST_DATATYPE;
-    if ((srcGenericDescPtr->layout != RpptLayout::NCDHW) && (srcGenericDescPtr->layout != RpptLayout::NDHWC)) return RPP_ERROR_INVALID_SRC_LAYOUT;
-    if ((dstGenericDescPtr->layout != RpptLayout::NCDHW) && (dstGenericDescPtr->layout != RpptLayout::NDHWC)) return RPP_ERROR_INVALID_DST_LAYOUT;
-    if (srcGenericDescPtr->layout != dstGenericDescPtr->layout) return RPP_ERROR_INVALID_ARGUMENTS;
-
-    if ((srcGenericDescPtr->dataType == RpptDataType::F32) && (dstGenericDescPtr->dataType == RpptDataType::F32))
-    {
-        flip_voxel_f32_f32_host_tensor(reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(srcPtr) + srcGenericDescPtr->offsetInBytes),
-                                       srcGenericDescPtr,
-                                       reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(dstPtr) + dstGenericDescPtr->offsetInBytes),
-                                       dstGenericDescPtr,
-                                       horizontalTensor,
-                                       verticalTensor,
-                                       depthTensor,
-                                       roiGenericPtrSrc,
-                                       roiType,
-                                       layoutParams,
-                                       rpp::deref(rppHandle));
-    }
-    else if ((srcGenericDescPtr->dataType == RpptDataType::U8) && (dstGenericDescPtr->dataType == RpptDataType::U8))
-    {
-        flip_voxel_u8_u8_host_tensor(static_cast<Rpp8u*>(srcPtr) + srcGenericDescPtr->offsetInBytes,
-                                     srcGenericDescPtr,
-                                     static_cast<Rpp8u*>(dstPtr) + dstGenericDescPtr->offsetInBytes,
-                                     dstGenericDescPtr,
-                                     horizontalTensor,
-                                     verticalTensor,
-                                     depthTensor,
-                                     roiGenericPtrSrc,
-                                     roiType,
-                                     layoutParams,
-                                     rpp::deref(rppHandle));
-    }
-
-    return RPP_SUCCESS;
-}
-
 /********************************************************************************************************************/
 /*********************************************** RPP_GPU_SUPPORT = ON ***********************************************/
 /********************************************************************************************************************/
