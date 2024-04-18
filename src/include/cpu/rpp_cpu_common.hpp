2b80df8e5b773becadd694254e693969e4b15b2c
Snehaa Giridharan <snehaa@multicorewareinc.com>
2024-04-18 10:10:54 -0400

    Revert changes in common file

--- src/include/cpu/rpp_cpu_common.hpp
+++ src/include/cpu/rpp_cpu_common.hpp
@@ -5006,8 +5006,8 @@ inline void compute_generic_bilinear_srclocs_and_interpolate(T *srcPtrChannel, R
 inline void compute_generic_bilinear_srclocs_1c_avx(__m256 &pSrcY, __m256 &pSrcX, RpptBilinearNbhoodLocsVecLen8 &srcLocs, __m256 *pBilinearCoeffs, __m256 &pSrcStrideH, __m256i *pxSrcStridesCHW, __m256 *pRoiLTRB)
 {
     __m256 pWeightParams[4], pSrcBilinearLTyx[4];
-    pSrcBilinearLTyx[0] = _mm256_floor_ps(pSrcY);     // srcLT->y = (Rpp32s) srcY;
-    pSrcBilinearLTyx[1] = _mm256_floor_ps(pSrcX);     // srcLT->x = (Rpp32s) srcX;
+    pSrcBilinearLTyx[0] = _mm256_floor_ps(pSrcY);                               // srcLT->y = (Rpp32s) srcY;
+    pSrcBilinearLTyx[1] = _mm256_floor_ps(pSrcX);                               // srcLT->x = (Rpp32s) srcX;
     pWeightParams[0] = _mm256_sub_ps(pSrcY, pSrcBilinearLTyx[0]);               // weightParams[0] = srcY - srcLT->y;
     pWeightParams[1] = _mm256_sub_ps(avx_p1, pWeightParams[0]);                 // weightParams[1] = 1 - weightParams[0];
     pWeightParams[2] = _mm256_sub_ps(pSrcX, pSrcBilinearLTyx[1]);               // weightParams[2] = srcX - srcLT->x;
