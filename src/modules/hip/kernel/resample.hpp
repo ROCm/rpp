2b11c5723e71b0a1fc89db7b6e08b0ffdc29ef2a
Snehaa Giridharan <snehaa@multicorewareinc.com>
2024-04-24 04:15:45 -0400

    Support multi channel resample

--- src/modules/hip/kernel/resample.hpp
+++ src/modules/hip/kernel/resample.hpp
@@ -2,7 +2,7 @@
 #include "rpp_hip_common.hpp"
 
 // -------------------- Set 0 - resample kernel --------------------
-
+// Single channel resample support
 __global__ void resample_1channel_hip_tensor(float *srcPtr,
                                              float *dstPtr,
                                              float inRateTensor,
@@ -44,6 +44,57 @@ __global__ void resample_1channel_hip_tensor(float *srcPtr,
             accum += srcPtr[inBlockRounded + locInWindow] * w;
         }
         dstPtr[outPos] = accum;
+    }
+}
+
+// Generic n channel resample support
+__global__ void resample_nchannel_hip_tensor(float *srcPtr,
+                                             float *dstPtr,
+                                             float inRateTensor,
+                                             float outRateTensor,
+                                             int srcLength,
+                                             int outEnd,
+                                             double scale,
+                                             RpptResamplingWindow &window,
+                                             int numChannels)
+{
+    int id_x = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
+
+    int block = 256;
+    int outBlock = id_x * block;
+    if (outBlock >= outEnd)
+        return;
+
+    int blockEnd = std::min(outBlock + block, outEnd);
+    double inBlockRaw = outBlock * scale;
+    int inBlockRounded = static_cast<int>(inBlockRaw);
+    float inPos = inBlockRaw - inBlockRounded;
+    float fscale = scale;
+    float tempBuf[numChannels] = 0.0f;
+
+    for (int outPos = outBlock; outPos < blockEnd; outPos++, inPos += fscale)
+    {
+        int loc0, loc1;
+        window.input_range(inPos, &loc0, &loc1);
+
+        if (loc0 + inBlockRounded < 0)
+            loc0 = -inBlockRounded;
+        if (loc1 + inBlockRounded > srcLength)
+            loc1 = srcLength - inBlockRounded;
+        int locInWindow = loc0;
+        float locBegin = locInWindow - inPos;
+        int ofs0 = loc0 * numChannels;
+        int ofs1 = loc1 * numChannels;
+
+        for (inOfs = ofs0; inOfs < ofs1; inOfs += numChannels, locBegin++)
+        {
+            float w = window(locBegin);
+            for (int c = 0; c < numChannels; c++)
+                tempBuf[c] += srcPtr[(inBlockRounded * numChannels) + inOfs + c] * w;
+        }
+        int dstLoc = outPos * numChannels;
+        for (int c = 0; c < numChannels; c++)
+            dstPtrTemp[dstLoc + c] = tempBuf[c];
     }
 }
 
@@ -97,6 +148,27 @@ RppStatus hip_exec_resample_tensor(Rpp32f *srcPtr,
                                    outEnd,
                                    scale,
                                    window);
+            }
+            else
+            {
+                int globalThreads_x = length;
+                int globalThreads_y = 1;
+                int globalThreads_z = 1;
+
+                hipLaunchKernelGGL(resample_nchannel_hip_tensor,
+                                   dim3(ceil((float)globalThreads_x/LOCAL_THREADS_X_1DIM), ceil((float)globalThreads_y/LOCAL_THREADS_Y_1DIM), ceil((float)globalThreads_z/LOCAL_THREADS_Z_1DIM)),
+                                   dim3(LOCAL_THREADS_X_1DIM, LOCAL_THREADS_Y_1DIM, LOCAL_THREADS_Z_1DIM),
+                                   0,
+                                   handle.GetStream(),
+                                   srcPtr + i * srcDescPtr->strides.nStride,
+                                   dstPtr + i * dstDescPtr->strides.nStride,
+                                   inRate,
+                                   outRate,
+                                   srcLength,
+                                   outEnd,
+                                   scale,
+                                   window,
+                                   numChannels);
             }
         }
 
