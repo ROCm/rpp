6f3b1d2d3e0da11d02c2634f737529f8d5b64df0
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2024-05-06 11:55:42 +0000

    added support to process first value in each column

--- src/modules/cpu/kernel/box_filter.hpp
+++ src/modules/cpu/kernel/box_filter.hpp
@@ -40,6 +40,24 @@ Rpp32f get_result(Rpp8u *srcPtrTemp1, Rpp8u *srcPtrTemp2, Rpp8u *srcPtrTemp3, Rp
     return accum;
 }
 
+void process_column(Rpp8u *srcPtrTemp1, Rpp8u *srcPtrTemp2, Rpp8u *srcPtrTemp3, Rpp8u *dstPtrTemp,
+                    bool firstRow, bool lastRow, Rpp16s convolutionFactor)
+{
+    Rpp32f sum = 0;
+    if (firstRow || lastRow)
+    {
+        sum += (static_cast<Rpp32f>(srcPtrTemp1[0]) + static_cast<Rpp32f>(srcPtrTemp1[1]));
+        sum += (static_cast<Rpp32f>(srcPtrTemp2[0]) + static_cast<Rpp32f>(srcPtrTemp2[1]));
+    }
+    else
+    {
+        sum += (static_cast<Rpp32f>(srcPtrTemp1[0]) + static_cast<Rpp32f>(srcPtrTemp1[1]));
+        sum += (static_cast<Rpp32f>(srcPtrTemp2[0]) + static_cast<Rpp32f>(srcPtrTemp2[1]));
+        sum += (static_cast<Rpp32f>(srcPtrTemp3[0]) + static_cast<Rpp32f>(srcPtrTemp3[1]));
+    }
+    sum *= 0.1111111f;
+    *dstPtrTemp = static_cast<Rpp8u>(RPPPIXELCHECK(sum));
+}
 
 RppStatus box_filter_u8_u8_host_tensor(Rpp8u *srcPtr,
                                        RpptDescPtr srcDescPtr,
@@ -77,14 +95,14 @@ RppStatus box_filter_u8_u8_host_tensor(Rpp8u *srcPtr,
         srcPtrChannel = srcPtrImage + (roi.xywhROI.xy.y * srcDescPtr->strides.hStride) + (roi.xywhROI.xy.x * layoutParams.bufferMultiplier);
         dstPtrChannel = dstPtrImage;
 
-        // Resize with fused output-layout toggle (NHWC -> NHWC)
+        // box filter without fused output-layout toggle (NCHW -> NCHW)
         if ((srcDescPtr->c == 1) && (srcDescPtr->layout == RpptLayout::NCHW) && (dstDescPtr->layout == RpptLayout::NCHW))
         {
             Rpp8u *srcPtrRow1, *srcPtrRow2, *srcPtrRow3, *dstPtrRow;
             srcPtrRow1 = srcPtrChannel;
             srcPtrRow2 = srcPtrRow1 + padLength * srcDescPtr->strides.hStride;
             srcPtrRow3 = srcPtrRow2 + padLength * srcDescPtr->strides.hStride;
-            dstPtrRow = dstPtrChannel + padLength;
+            dstPtrRow = dstPtrChannel;
 
             Rpp32u alignedLength = ((bufferLength - 2 * padLength) / 12) * 12;
             const __m128i xmm_pxMask02To15 = _mm_setr_epi8(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0x80, 0x80);
@@ -101,14 +119,17 @@ RppStatus box_filter_u8_u8_host_tensor(Rpp8u *srcPtr,
                 Rpp8u *srcPtrTemp2 = srcPtrRow2;
                 Rpp8u *srcPtrTemp3 = srcPtrRow3;
                 Rpp8u *dstPtrTemp = dstPtrRow;
+                process_column(srcPtrTemp1, srcPtrTemp2, srcPtrTemp3, dstPtrTemp,
+                               firstRow, lastRow, convolutionFactor);
+                dstPtrTemp++;
                 for (; vectorLoopCount < alignedLength; vectorLoopCount += 12)
                 {
                     __m128i pxRow[3];
                     if (!firstRow && !lastRow)
                     {
-                        pxRow[0] = _mm_loadu_si128((__m128i *)srcPtrTemp1);
-                        pxRow[1] = _mm_loadu_si128((__m128i *)srcPtrTemp2);
-                        pxRow[2] = _mm_loadu_si128((__m128i *)srcPtrTemp3);
+                        pxRow[0] = _mm_loadu_si128((__m128i *)srcPtrTemp1);    // load ROW[0][0] .. ROW[0][15]
+                        pxRow[1] = _mm_loadu_si128((__m128i *)srcPtrTemp2);    // load ROW[1][0] .. ROW[1][15]
+                        pxRow[2] = _mm_loadu_si128((__m128i *)srcPtrTemp3);    // load ROW[2][0] .. ROW[2][15]
                     }
                     else
                     {
@@ -118,28 +139,28 @@ RppStatus box_filter_u8_u8_host_tensor(Rpp8u *srcPtr,
                     }
 
                     __m128i pxUpper, pxLower;
-                    pxUpper = _mm_unpacklo_epi8(pxRow[0], xmm_px0);
-                    pxUpper = _mm_add_epi16(pxUpper, _mm_unpacklo_epi8(pxRow[1], xmm_px0));
-                    pxUpper = _mm_add_epi16(pxUpper, _mm_unpacklo_epi8(pxRow[2], xmm_px0));
+                    pxUpper = _mm_unpacklo_epi8(pxRow[0], xmm_px0);                            // ROW0 0-7
+                    pxUpper = _mm_add_epi16(pxUpper, _mm_unpacklo_epi8(pxRow[1], xmm_px0));    // ROW0 0-7 + ROW1 0-7
+                    pxUpper = _mm_add_epi16(pxUpper, _mm_unpacklo_epi8(pxRow[2], xmm_px0));    // upper accum - ROW0 0-7 + ROW1 0-7 + ROW2 0-7
 
-                    pxLower = _mm_unpackhi_epi8(pxRow[0], xmm_px0);
-                    pxLower = _mm_add_epi16(pxLower, _mm_unpackhi_epi8(pxRow[1], xmm_px0));
-                    pxLower = _mm_add_epi16(pxLower, _mm_unpackhi_epi8(pxRow[2], xmm_px0));
+                    pxLower = _mm_unpackhi_epi8(pxRow[0], xmm_px0);                            // ROW0 8-15
+                    pxLower = _mm_add_epi16(pxLower, _mm_unpackhi_epi8(pxRow[1], xmm_px0));    // ROW0 8-15 + ROW1 8-15
+                    pxLower = _mm_add_epi16(pxLower, _mm_unpackhi_epi8(pxRow[2], xmm_px0));    // lower accum - ROW0 8-15 + ROW1 8-15 + ROW2 8-15
 
                     // shuffle to get the correct order
                     __m128i pxShift[4];
-                    pxShift[0] = _mm_shuffle_epi8(pxUpper, xmm_pxMask02To15);
-                    pxShift[0] = _mm_add_epi16(pxShift[0], _mm_shuffle_epi8(pxLower, xmm_pxMask00To01));
-                    pxShift[1] = _mm_shuffle_epi8(pxUpper, xmm_pxMask04To15);
-                    pxShift[1] = _mm_add_epi16(pxShift[1], _mm_shuffle_epi8(pxLower, xmm_pxMask00To03));
+                    pxShift[0] = _mm_shuffle_epi8(pxUpper, xmm_pxMask02To15); // upper accum 1-7
+                    pxShift[0] = _mm_add_epi16(pxShift[0], _mm_shuffle_epi8(pxLower, xmm_pxMask00To01)); // upper accum 1-7 + lower accum 0
+                    pxShift[1] = _mm_shuffle_epi8(pxUpper, xmm_pxMask04To15); // upper accum 2-7
+                    pxShift[1] = _mm_add_epi16(pxShift[1], _mm_shuffle_epi8(pxLower, xmm_pxMask00To03)); // upper accum 2-7 + lower accum 0-2
 
-                    pxShift[2] = _mm_shuffle_epi8(pxLower, xmm_pxMask02To15);
-                    pxShift[3] = _mm_shuffle_epi8(pxLower, xmm_pxMask04To15);
+                    pxShift[2] = _mm_shuffle_epi8(pxLower, xmm_pxMask02To15); // lower accum 1-7
+                    pxShift[3] = _mm_shuffle_epi8(pxLower, xmm_pxMask04To15); // lower accum 2-7
 
                     __m128i pxResult[2];
-                    pxResult[0] = _mm_add_epi16(_mm_add_epi16(pxUpper, pxShift[0]), pxShift[1]);
+                    pxResult[0] = _mm_add_epi16(_mm_add_epi16(pxUpper, pxShift[0]), pxShift[1]); // upper accum 0-7 + upper accum 1-7 + upper accum 2-7 + lower accum 0-2
                     pxResult[0] = _mm_mulhi_epi16(pxResult[0], pxConvolutionFactor);
-                    pxResult[1] = _mm_add_epi16(_mm_add_epi16(pxLower, pxShift[2]), pxShift[3]);
+                    pxResult[1] = _mm_add_epi16(_mm_add_epi16(pxLower, pxShift[2]), pxShift[3]); // lower accum 0-7 + lower accum 1-7 + lower accum 2-7
                     pxResult[1] = _mm_mulhi_epi16(pxResult[1], pxConvolutionFactor);
                     pxResult[0] = _mm_packus_epi16(pxResult[0], pxResult[1]);
                     _mm_storeu_si128((__m128i *)dstPtrTemp, pxResult[0]);
