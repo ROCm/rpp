6bbfad152f3b4de61921c34318eb25ffd7f387c2
kiritigowda <kiritigowda@gmail.com>
2024-08-13 10:43:34 -0700

    Version - Add note

--- include/rpp_version.h
+++ include/rpp_version.h
@@ -37,6 +37,7 @@ SOFTWARE.
 #ifdef __cplusplus
 extern "C" {
 #endif
+// NOTE: Match the version with CMakelists.txt version
 #define RPP_VERSION_MAJOR 1
 #define RPP_VERSION_MINOR 9
 #define RPP_VERSION_PATCH 0
