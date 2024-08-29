161b85bc3d544dd25009b66d9c05b0901c286812
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2024-08-29 12:26:31 +0000

    added version changes

--- include/rpp_version.h
+++ include/rpp_version.h
@@ -40,7 +40,7 @@ extern "C" {
 // NOTE: IMPORTANT: Match the version with CMakelists.txt version
 #define RPP_VERSION_MAJOR 1
 #define RPP_VERSION_MINOR 9
-#define RPP_VERSION_PATCH 1
+#define RPP_VERSION_PATCH 4
 #ifdef __cplusplus
 }
 #endif
