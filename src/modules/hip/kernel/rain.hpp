32799edd6857ea75db1fb5e1535f72a0f2252ec9
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2024-08-30 07:57:20 +0000

    Modified RGB Rain Mask to planar Rain Mask in HIP

--- src/modules/hip/kernel/rain.hpp
+++ src/modules/hip/kernel/rain.hpp
@@ -10,13 +10,14 @@ __device__ __forceinline__ void rain_hip_compute(d_float8 *src1_f8, d_float8 *sr
 template <typename T>
 __global__ void rain_pkd_hip_tensor(T *srcPtr1,
                                     T *srcPtr2,
-                                    uint2 srcStridesNH,
+                                    uint3 srcStridesNHW,
                                     T *dstPtr,
                                     uint2 dstStridesNH,
                                     float *alpha,
                                     RpptROIPtr roiTensorPtrSrc)
 {
-    int id_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 8;
+    int id_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 24;
+    int maskid_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 8;
     int id_y = hipBlockIdx_y * hipBlockDim_y + hipThreadIdx_y;
     int id_z = hipBlockIdx_z * hipBlockDim_z + hipThreadIdx_z;
 
@@ -25,18 +26,21 @@ __global__ void rain_pkd_hip_tensor(T *srcPtr1,
         return;
     }
 
-    uint srcIdx1 = (id_z * srcStridesNH.x) + ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNH.y) + (id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x * 3);
-    uint srcIdx2 = ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNH.y) + (id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x * 3);
+    uint srcIdx1 = (id_z * srcStridesNHW.x) + ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNHW.y) + (id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x * 3);
+    uint srcIdx2 = ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNHW.z) + (maskid_x + roiTensorPtrSrc[id_z].xywhROI.xy.x);
     uint dstIdx = (id_z * dstStridesNH.x) + (id_y * dstStridesNH.y) + id_x;
 
     float4 alpha_f4 = (float4)alpha[id_z];
 
-    d_float8 src1_f8, src2_f8, dst_f8;
+    d_float24 src1_f24, dst_f24;
+    d_float8 src2_f8;
 
-    rpp_hip_load8_and_unpack_to_float8(srcPtr1 + srcIdx1, &src1_f8);
+    rpp_hip_load24_pkd3_and_unpack_to_float24_pln3(srcPtr1 + srcIdx1, &src1_f24);
     rpp_hip_load8_and_unpack_to_float8(srcPtr2 + srcIdx2, &src2_f8);
-    rain_hip_compute(&src1_f8, &src2_f8, &dst_f8, &alpha_f4);
-    rpp_hip_pack_float8_and_store8(dstPtr + dstIdx, &dst_f8);
+    rain_hip_compute(&src1_f24.f8[0], &src2_f8, &dst_f24.f8[0], &alpha_f4);
+    rain_hip_compute(&src1_f24.f8[1], &src2_f8, &dst_f24.f8[1], &alpha_f4);
+    rain_hip_compute(&src1_f24.f8[2], &src2_f8, &dst_f24.f8[2], &alpha_f4);
+    rpp_hip_pack_float24_pln3_and_store24_pkd3(dstPtr + dstIdx, &dst_f24);
 }
 
 template <typename T>
@@ -74,20 +78,16 @@ __global__ void rain_pln_hip_tensor(T *srcPtr1,
     if (channelsDst == 3)
     {
         srcIdx1 += srcStridesNCH.y;
-        srcIdx2 += srcStridesNCH.y;
         dstIdx += dstStridesNCH.y;
 
         rpp_hip_load8_and_unpack_to_float8(srcPtr1 + srcIdx1, &src1_f8);
-        rpp_hip_load8_and_unpack_to_float8(srcPtr2 + srcIdx2, &src2_f8);
         rain_hip_compute(&src1_f8, &src2_f8, &dst_f8, &alpha_f4);
         rpp_hip_pack_float8_and_store8(dstPtr + dstIdx, &dst_f8);
 
         srcIdx1 += srcStridesNCH.y;
-        srcIdx2 += srcStridesNCH.y;
         dstIdx += dstStridesNCH.y;
 
         rpp_hip_load8_and_unpack_to_float8(srcPtr1 + srcIdx1, &src1_f8);
-        rpp_hip_load8_and_unpack_to_float8(srcPtr2 + srcIdx2, &src2_f8);
         rain_hip_compute(&src1_f8, &src2_f8, &dst_f8, &alpha_f4);
         rpp_hip_pack_float8_and_store8(dstPtr + dstIdx, &dst_f8);
     }
@@ -96,7 +96,7 @@ __global__ void rain_pln_hip_tensor(T *srcPtr1,
 template <typename T>
 __global__ void rain_pkd3_pln3_hip_tensor(T *srcPtr1,
                                           T *srcPtr2,
-                                          uint2 srcStridesNH,
+                                          uint3 srcStridesNHW,
                                           T *dstPtr,
                                           uint3 dstStridesNCH,
                                           float *alpha,
@@ -111,19 +111,20 @@ __global__ void rain_pkd3_pln3_hip_tensor(T *srcPtr1,
         return;
     }
 
-    uint srcIdx1 = (id_z * srcStridesNH.x) + ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNH.y) + ((id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x) * 3);
-    uint srcIdx2 = ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNH.y) + ((id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x) * 3);
+    uint srcIdx1 = (id_z * srcStridesNHW.x) + ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNHW.y) + ((id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x) * 3);
+    uint srcIdx2 = ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNHW.z) + (id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x);
     uint dstIdx = (id_z * dstStridesNCH.x) + (id_y * dstStridesNCH.z) + id_x;
 
     float4 alpha_f4 = (float4)alpha[id_z];
 
-    d_float24 src1_f24, src2_f24, dst_f24;
+    d_float24 src1_f24, dst_f24;
+    d_float8 src2_f8;
 
     rpp_hip_load24_pkd3_and_unpack_to_float24_pln3(srcPtr1 + srcIdx1, &src1_f24);
-    rpp_hip_load24_pkd3_and_unpack_to_float24_pln3(srcPtr2 + srcIdx2, &src2_f24);
-    rain_hip_compute(&src1_f24.f8[0], &src2_f24.f8[0], &dst_f24.f8[0], &alpha_f4);
-    rain_hip_compute(&src1_f24.f8[1], &src2_f24.f8[1], &dst_f24.f8[1], &alpha_f4);
-    rain_hip_compute(&src1_f24.f8[2], &src2_f24.f8[2], &dst_f24.f8[2], &alpha_f4);
+    rpp_hip_load8_and_unpack_to_float8(srcPtr2 + srcIdx2, &src2_f8);
+    rain_hip_compute(&src1_f24.f8[0], &src2_f8, &dst_f24.f8[0], &alpha_f4);
+    rain_hip_compute(&src1_f24.f8[1], &src2_f8, &dst_f24.f8[1], &alpha_f4);
+    rain_hip_compute(&src1_f24.f8[2], &src2_f8, &dst_f24.f8[2], &alpha_f4);
     rpp_hip_pack_float24_pln3_and_store24_pln3(dstPtr + dstIdx, dstStridesNCH.y, &dst_f24);
 }
 
@@ -151,14 +152,15 @@ __global__ void rain_pln3_pkd3_hip_tensor(T *srcPtr1,
 
     float4 alpha_f4 = (float4)(alpha[id_z]);
 
-    d_float24 src1_f24, src2_f24, dst_f24;
+    d_float24 src1_f24, dst_f24;
+    d_float8 src2_f8;
 
-    rpp_hip_load24_pln3_and_unpack_to_float24_pkd3(srcPtr1 + srcIdx1, srcStridesNCH.y, &src1_f24);
-    rpp_hip_load24_pln3_and_unpack_to_float24_pkd3(srcPtr2 + srcIdx2, srcStridesNCH.y, &src2_f24);
-    rain_hip_compute(&src1_f24.f8[0], &src2_f24.f8[0], &dst_f24.f8[0], &alpha_f4);
-    rain_hip_compute(&src1_f24.f8[1], &src2_f24.f8[1], &dst_f24.f8[1], &alpha_f4);
-    rain_hip_compute(&src1_f24.f8[2], &src2_f24.f8[2], &dst_f24.f8[2], &alpha_f4);
-    rpp_hip_pack_float24_pkd3_and_store24_pkd3(dstPtr + dstIdx, &dst_f24);
+    rpp_hip_load24_pln3_and_unpack_to_float24_pln3(srcPtr1 + srcIdx1, srcStridesNCH.y, &src1_f24);
+    rpp_hip_load8_and_unpack_to_float8(srcPtr2 + srcIdx2, &src2_f8);
+    rain_hip_compute(&src1_f24.f8[0], &src2_f8, &dst_f24.f8[0], &alpha_f4);
+    rain_hip_compute(&src1_f24.f8[1], &src2_f8, &dst_f24.f8[1], &alpha_f4);
+    rain_hip_compute(&src1_f24.f8[2], &src2_f8, &dst_f24.f8[2], &alpha_f4);
+    rpp_hip_pack_float24_pln3_and_store24_pkd3(dstPtr + dstIdx, &dst_f24);
 }
 
 template <typename T>
@@ -199,7 +201,7 @@ RppStatus hip_exec_rain_tensor(T *srcPtr1,
 
             if (x >= 0 && x < srcDescPtr->w && y < srcDescPtr->h)
             {
-                T *rainLayerTemp = rainLayer + y * srcDescPtr->strides.hStride + x * srcDescPtr->strides.wStride;
+                T *rainLayerTemp = rainLayer + y * srcDescPtr->w + x;
 
                 // Conditionally assign rain value based on type T
                 T rainValue;
@@ -217,17 +219,12 @@ RppStatus hip_exec_rain_tensor(T *srcPtr1,
                 }
 
                 *rainLayerTemp = rainValue;
-                if (srcDescPtr->c > 1)
-                {
-                    *(rainLayerTemp + srcDescPtr->strides.cStride) = rainValue;
-                    *(rainLayerTemp + 2 * srcDescPtr->strides.cStride) = rainValue;
-                }
             }
         }
     }
 
     T *rainLayerHip = reinterpret_cast<T *>(handle.GetInitHandle()->mem.mgpu.scratchBufferHip.floatmem);
-    CHECK_RETURN_STATUS(hipMemcpyAsync(rainLayerHip, rainLayer, srcDescPtr->strides.nStride * sizeof(T), hipMemcpyHostToDevice, handle.GetStream()));
+    CHECK_RETURN_STATUS(hipMemcpyAsync(rainLayerHip, rainLayer, srcDescPtr->w * srcDescPtr->h * sizeof(T), hipMemcpyHostToDevice, handle.GetStream()));
 
     int globalThreads_x = (dstDescPtr->strides.hStride + 7) >> 3;
     int globalThreads_y = dstDescPtr->h;
@@ -235,6 +232,7 @@ RppStatus hip_exec_rain_tensor(T *srcPtr1,
 
     if ((srcDescPtr->layout == RpptLayout::NHWC) && (dstDescPtr->layout == RpptLayout::NHWC))
     {
+        globalThreads_x = (dstDescPtr->w + 7) >> 3;
         hipLaunchKernelGGL(rain_pkd_hip_tensor,
                            dim3(ceil((float)globalThreads_x/LOCAL_THREADS_X), ceil((float)globalThreads_y/LOCAL_THREADS_Y), ceil((float)globalThreads_z/LOCAL_THREADS_Z)),
                            dim3(LOCAL_THREADS_X, LOCAL_THREADS_Y, LOCAL_THREADS_Z),
@@ -242,7 +240,7 @@ RppStatus hip_exec_rain_tensor(T *srcPtr1,
                            handle.GetStream(),
                            srcPtr1,
                            rainLayerHip,
-                           make_uint2(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride),
+                           make_uint3(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride, srcDescPtr->w),
                            dstPtr,
                            make_uint2(dstDescPtr->strides.nStride, dstDescPtr->strides.hStride),
                            alpha,
@@ -275,7 +273,7 @@ RppStatus hip_exec_rain_tensor(T *srcPtr1,
                                handle.GetStream(),
                                srcPtr1,
                                rainLayerHip,
-                               make_uint2(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride),
+                               make_uint3(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride, srcDescPtr->w),
                                dstPtr,
                                make_uint3(dstDescPtr->strides.nStride, dstDescPtr->strides.cStride, dstDescPtr->strides.hStride),
                                alpha,
