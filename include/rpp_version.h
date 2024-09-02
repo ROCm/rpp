4e3cd9a792acc4df957507d52332a4eb027efe55
Srihari-mcw <srihari@multicorewareinc.com>
2024-09-02 18:46:52 +0530

    Update the version

--- include/rpp_version.h
+++ include/rpp_version.h
@@ -39,8 +39,8 @@ extern "C" {
 #endif
 // NOTE: IMPORTANT: Match the version with CMakelists.txt version
 #define RPP_VERSION_MAJOR 1
-#define RPP_VERSION_MINOR 9
-#define RPP_VERSION_PATCH 1
+#define RPP_VERSION_MINOR 14
+#define RPP_VERSION_PATCH 0
 #ifdef __cplusplus
 }
 #endif
