30bd007b2dc98c798a28bbcc922903deeceae9c3
Srihari-mcw <srihari@multicorewareinc.com>
2024-09-18 00:06:16 +0530

    More updates

--- src/modules/hip/kernel/exclusive_or.hpp
+++ src/modules/hip/kernel/exclusive_or.hpp
@@ -1,8 +1,8 @@
 #include <hip/hip_runtime.h>
 #include "rpp_hip_common.hpp"
 
-/*ExclusiveXOR is logical operation only on U8/I8 types.
-   For a Rpp32f precision image (pixel values from 0-1), the ExclusiveXOR is applied on a 0-255
+/* ExclusiveOR is logical operation only on U8/I8 types.
+   For a Rpp32f precision image (pixel values from 0-1), the ExclusiveOR is applied on a 0-255
    range-translated approximation, of the original 0-1 decimal-range image.
    The bitwise operation is applied to the char representation of the raw floating-point data in memory */
 
