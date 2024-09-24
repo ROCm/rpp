d52e53dad4eb19353a0d7e9819b1ab915cdd3e3e
Srihari-mcw <srihari@multicorewareinc.com>
2024-09-24 07:29:40 +0530

    Update separate code for PLN3 to PLN3 I8

--- src/modules/cpu/kernel/exclusive_or.hpp
+++ src/modules/cpu/kernel/exclusive_or.hpp
@@ -978,58 +978,130 @@ RppStatus exclusive_or_i8_i8_host_tensor(Rpp8s *srcPtr1,
                 dstPtrRow += dstDescPtr->strides.hStride;
             }
         }
+        else if ((srcDescPtr->c == 3) && (srcDescPtr->layout == RpptLayout::NCHW) && (dstDescPtr->layout == RpptLayout::NCHW))
+        {
+            Rpp8s *srcPtr1RowR, *srcPtr1RowG, *srcPtr1RowB, *srcPtr2RowR, *srcPtr2RowG, *srcPtr2RowB, *dstPtrRowR, *dstPtrRowG, *dstPtrRowB;
+            srcPtr1RowR = srcPtr1Channel;
+            srcPtr1RowG = srcPtr1RowR + srcDescPtr->strides.cStride;
+            srcPtr1RowB = srcPtr1RowG + srcDescPtr->strides.cStride;
+            srcPtr2RowR = srcPtr2Channel;
+            srcPtr2RowG = srcPtr2RowR + srcDescPtr->strides.cStride;
+            srcPtr2RowB = srcPtr2RowG + srcDescPtr->strides.cStride;
+            dstPtrRowR = dstPtrChannel;
+            dstPtrRowG = dstPtrRowR + dstDescPtr->strides.cStride;
+            dstPtrRowB = dstPtrRowG + dstDescPtr->strides.cStride;
+            for (int i = 0; i < roi.xywhROI.roiHeight; i++)
+            {
+                Rpp8u *srcPtr1TempR, *srcPtr1TempG, *srcPtr1TempB, *srcPtr2TempR, *srcPtr2TempG, *srcPtr2TempB, *dstPtrTempR, *dstPtrTempG, *dstPtrTempB;
+                srcPtr1TempR = srcPtr1RowR;
+                srcPtr1TempG = srcPtr1RowG;
+                srcPtr1TempB = srcPtr1RowB;
+                srcPtr2TempR = srcPtr2RowR;
+                srcPtr2TempG = srcPtr2RowG;
+                srcPtr2TempB = srcPtr2RowB;
+                dstPtrTempR = dstPtrRowR;
+                dstPtrTempG = dstPtrRowG;
+                dstPtrTempB = dstPtrRowB;
 
+                int vectorLoopCount = 0;
+                for (; vectorLoopCount < alignedLength; vectorLoopCount += vectorIncrementPerChannel)
+                {
+                    __m256i p1[3], p2[3];
+
+                    rpp_simd_load(rpp_load96_i8pln3_to_u8pln3, srcPtr1TempR, srcPtr1TempG, srcPtr1TempB, p1);    // simd loads
+                    rpp_simd_load(rpp_load96_i8pln3_to_u8pln3, srcPtr2TempR, srcPtr2TempG, srcPtr2TempB, p2);    // simd loads
+                    p1[0] = _mm256_xor_si256(p1[0], p2[0]);    // exclusive_or computation
+                    p1[1] = _mm256_xor_si256(p1[1], p2[1]);    // exclusive_or computation
+                    p1[2] = _mm256_xor_si256(p1[2], p2[2]);    // exclusive_or computation
+                    rpp_simd_store(rpp_store96_u8pln3_to_i8pln3, dstPtrTempR, dstPtrTempG, dstPtrTempB, p1);
+
+                    srcPtr1TempR += vectorIncrementPerChannel;
+                    srcPtr1TempG += vectorIncrementPerChannel;
+                    srcPtr1TempB += vectorIncrementPerChannel;
+                    srcPtr2TempR += vectorIncrementPerChannel;
+                    srcPtr2TempG += vectorIncrementPerChannel;
+                    srcPtr2TempB += vectorIncrementPerChannel;
+                    dstPtrTempR += vectorIncrementPerChannel;
+                    dstPtrTempG += vectorIncrementPerChannel;
+                    dstPtrTempB += vectorIncrementPerChannel;
+                }
+
+                for (; vectorLoopCount < bufferLength; vectorLoopCount++)
+                {
+                    *dstPtrTempR = static_cast<Rpp8s>(RPPPIXELCHECKI8(((static_cast<Rpp8u>((*srcPtr1TempR +  128) ^ static_cast<Rpp8u>(*srcPtr2TempR +  128)))) -  128));
+                    *dstPtrTempG = static_cast<Rpp8s>(RPPPIXELCHECKI8(((static_cast<Rpp8u>((*srcPtr1TempG +  128) ^ static_cast<Rpp8u>(*srcPtr2TempG +  128)))) -  128));
+                    *dstPtrTempB = static_cast<Rpp8s>(RPPPIXELCHECKI8(((static_cast<Rpp8u>((*srcPtr1TempB +  128) ^ static_cast<Rpp8u>(*srcPtr2TempB +  128)))) -  128));
+
+                    srcPtr1TempR++;
+                    srcPtr1TempG++;
+                    srcPtr1TempB++;
+                    srcPtr2TempR++;
+                    srcPtr2TempG++;
+                    srcPtr2TempB++;
+                    dstPtrTempR++;
+                    dstPtrTempG++;
+                    dstPtrTempB++;
+                }
+
+                srcPtr1RowR += srcDescPtr->strides.hStride;
+                srcPtr1RowG += srcDescPtr->strides.hStride;
+                srcPtr1RowB += srcDescPtr->strides.hStride;
+                srcPtr2RowR += srcDescPtr->strides.hStride;
+                srcPtr2RowG += srcDescPtr->strides.hStride;
+                srcPtr2RowB += srcDescPtr->strides.hStride;
+                dstPtrTempR += dstDescPtr->strides.hStride;
+                dstPtrTempG += dstDescPtr->strides.hStride;
+                dstPtrTempB += dstDescPtr->strides.hStride;
+            }
+        }
         // Exclusive OR without fused output-layout toggle (NHWC -> NHWC or NCHW -> NCHW)
         else
         {
             alignedLength = bufferLength & ~31;
 
-            for (int c = 0; c < layoutParams.channelParam; c++)
+            Rpp8s *srcPtr1Row, *srcPtr2Row, *dstPtrRow;
+            srcPtr1Row = srcPtr1Channel;
+            srcPtr2Row = srcPtr2Channel;
+            dstPtrRow = dstPtrChannel;
+
+            for (int i = 0; i < roi.xywhROI.roiHeight; i++)
             {
-                Rpp8s *srcPtr1Row, *srcPtr2Row, *dstPtrRow;
-                srcPtr1Row = srcPtr1Channel;
-                srcPtr2Row = srcPtr2Channel;
-                dstPtrRow = dstPtrChannel;
+                Rpp8s *srcPtr1Temp, *srcPtr2Temp, *dstPtrTemp;
+                srcPtr1Temp = srcPtr1Row;
+                srcPtr2Temp = srcPtr2Row;
+                dstPtrTemp = dstPtrRow;
 
-                for (int i = 0; i < roi.xywhROI.roiHeight; i++)
+                int vectorLoopCount = 0;
+
+                for (; vectorLoopCount < alignedLength; vectorLoopCount += vectorIncrementPerChannel)
                 {
-                    Rpp8s *srcPtr1Temp, *srcPtr2Temp, *dstPtrTemp;
-                    srcPtr1Temp = srcPtr1Row;
-                    srcPtr2Temp = srcPtr2Row;
-                    dstPtrTemp = dstPtrRow;
+                    __m256i p1, p2;
 
-                    int vectorLoopCount = 0;
+                    p1 = _mm256_add_epi8(avx_pxConvertI8, _mm256_loadu_si256((__m256i *)srcPtr1Temp));   // simd loads
+                    p2 = _mm256_add_epi8(avx_pxConvertI8, _mm256_loadu_si256((__m256i *)srcPtr2Temp));   // simd loads
+                    p1 = _mm256_xor_si256(p1, p2);    // exclusive_or computation
+                    _mm256_storeu_si256((__m256i *)dstPtrTemp, _mm256_sub_epi8(p1, avx_pxConvertI8));    // simd stores
 
-                    for (; vectorLoopCount < alignedLength; vectorLoopCount += vectorIncrementPerChannel)
-                    {
-                        __m256i p1, p2;
+                    srcPtr1Temp += vectorIncrementPerChannel;
+                    srcPtr2Temp += vectorIncrementPerChannel;
+                    dstPtrTemp += vectorIncrementPerChannel;
+                }
+                for (; vectorLoopCount < bufferLength; vectorLoopCount++)
+                {
+                    *dstPtrTemp++ = static_cast<Rpp8s>(RPPPIXELCHECKI8(((static_cast<Rpp8u>((*srcPtr1Temp +  128) ^ static_cast<Rpp8u>(*srcPtr2Temp +  128)))) -  128));
 
-                        p1 = _mm256_add_epi8(avx_pxConvertI8, _mm256_loadu_si256((__m256i *)srcPtr1Temp));   // simd loads
-                        p2 = _mm256_add_epi8(avx_pxConvertI8, _mm256_loadu_si256((__m256i *)srcPtr2Temp));   // simd loads
-                        p1 = _mm256_xor_si256(p1, p2);    // exclusive_or computation
-                        _mm256_storeu_si256((__m256i *)dstPtrTemp, _mm256_sub_epi8(p1, avx_pxConvertI8));    // simd stores
-
-                        srcPtr1Temp += vectorIncrementPerChannel;
-                        srcPtr2Temp += vectorIncrementPerChannel;
-                        dstPtrTemp += vectorIncrementPerChannel;
-                    }
-                    for (; vectorLoopCount < bufferLength; vectorLoopCount++)
-                    {
-                        *dstPtrTemp++ = static_cast<Rpp8s>(RPPPIXELCHECKI8(((static_cast<Rpp8u>((*srcPtr1Temp +  128) ^ static_cast<Rpp8u>(*srcPtr2Temp +  128)))) -  128));
-
-                        srcPtr1Temp++;
-                        srcPtr2Temp++;
-                    }
-
-                    srcPtr1Row += srcDescPtr->strides.hStride;
-                    srcPtr2Row += srcDescPtr->strides.hStride;
-                    dstPtrRow += dstDescPtr->strides.hStride;
+                    srcPtr1Temp++;
+                    srcPtr2Temp++;
                 }
 
-                srcPtr1Channel += srcDescPtr->strides.cStride;
-                srcPtr2Channel += srcDescPtr->strides.cStride;
-                dstPtrChannel += dstDescPtr->strides.cStride;
+                srcPtr1Row += srcDescPtr->strides.hStride;
+                srcPtr2Row += srcDescPtr->strides.hStride;
+                dstPtrRow += dstDescPtr->strides.hStride;
             }
+
+            srcPtr1Channel += srcDescPtr->strides.cStride;
+            srcPtr2Channel += srcDescPtr->strides.cStride;
+            dstPtrChannel += dstDescPtr->strides.cStride;
         }
     }
 
