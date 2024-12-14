243f29b32185e7fc80a62a340c6024e3677e4cde
Kiriti Gowda <kiritigowda@gmail.com>
2024-12-13 18:32:00 -0800

    Package - Updates and bugfixes (#488)
    * Package - remove clang from test
    
    * CMakeLists - remove BUILD_WITH_AMD_ADVANCE
    
    * Package - Add OMP dependency
    
    * Find Packages - Updates
    
    * Test Package - Deps
    
    * Test - backend Info
    
    * Tests - Fix HIP Test Add
    
    * Test - Add HIP Path
    
    * Test - Find HIP Updates
    
    * Tests - Fix HIP compilation

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
     bool kernelSizeCase = (testCase == 40 || testCase == 41 || testCase == 49 || testCase == 54);
     bool dualInputCase = (testCase == 2 || testCase == 30 || testCase == 33 || testCase == 61 || testCase == 63 || testCase == 65 || testCase == 68);
     bool randomOutputCase = (testCase == 6 || testCase == 8 || testCase == 10 || testCase == 84 || testCase == 49 || testCase == 54);
-    bool nonQACase = (testCase == 24 || || testCase == 28 || testCase == 54);
+    bool nonQACase = (testCase == 24 || testCase == 28 || testCase == 54);
     bool interpolationTypeCase = (testCase == 21 || testCase == 23 || testCase == 24|| testCase == 28 || testCase == 79);
     bool reductionTypeCase = (testCase == 87 || testCase == 88 || testCase == 89 || testCase == 90 || testCase == 91);
     bool noiseTypeCase = (testCase == 8);
