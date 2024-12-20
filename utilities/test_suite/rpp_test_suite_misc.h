ecf1681db49f58d3d8116dbd556f2ad069128be3
dineshbabu-ravichandran <dineshbabu.ravichandran@multicorewareinc.com>
2024-12-20 07:25:03 +0000

    Minor changes on test_suite

--- utilities/test_suite/rpp_test_suite_misc.h
+++ utilities/test_suite/rpp_test_suite_misc.h
@@ -116,7 +116,7 @@ void fill_roi_values(Rpp32u nDim, Rpp32u batchSize, Rpp32u *roiTensor, bool qaMo
         {
             case 2:
             {
-                std::array<Rpp32u, 4> roi = {0, 0, 15, 15};
+                std::array<Rpp32u, 4> roi = {0, 0, 1920, 1080};
                 for(int i = 0, j = 0; i < batchSize ; i++, j += 4)
                     std::copy(roi.begin(), roi.end(), &roiTensor[j]);
                 break;
