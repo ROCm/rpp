5a2e5ff2db2563418f9f912acfc27b76e7e2600a
Srihari-mcw <srihari@multicorewareinc.com>
2025-01-20 18:56:13 +0530

    Cleanup and updated templated code until water

--- src/modules/cpu/kernel/transpose.hpp
+++ src/modules/cpu/kernel/transpose.hpp
@@ -25,99 +25,18 @@ SOFTWARE.
 #include "rppdefs.h"
 #include "rpp_cpu_common.hpp"
 
-template<typename T>
-void transpose_generic_nd_recursive(T *dst, Rpp32u *dstStrides, T *src, Rpp32u *srcStrides, Rpp32u *dstShape, Rpp32u tensorDims)
-{
-    // exit case for recursion
-    if (tensorDims == 0)
-    {
-        *dst = *src;
-    }
-    else
-    {
-        for (int i = 0; i < *dstShape; i++)
-        {
-            transpose_generic_nd_recursive(dst, dstStrides + 1, src, srcStrides + 1, dstShape + 1, tensorDims - 1);
-            dst += *dstStrides;
-            src += *srcStrides;
-        }
-    }
-}
-
-template<typename T>
-void transpose_generic_setup_and_run(T *srcPtrTemp, T *dstPtrTemp, Rpp32u *length, Rpp32u *perm, Rpp32u tensorDims)
-{
-    Rpp32u dstShape[RPPT_MAX_DIMS];
-    Rpp32u srcStrides[RPPT_MAX_DIMS];
-    Rpp32u dstStrides[RPPT_MAX_DIMS];
-
-    // compute output shape
-    for(Rpp32u i = 0; i < tensorDims; i++)
-        dstShape[i] = length[perm[i]];
-
-    // compute output strides
-    compute_strides(dstStrides, dstShape, tensorDims);
-
-    // compute input strides and update as per the permute order
-    Rpp32u tempStrides[RPPT_MAX_DIMS];
-    compute_strides(tempStrides, length, tensorDims);
-    for(int i = 0; i < tensorDims; i++)
-        srcStrides[i] = tempStrides[perm[i]];
-
-    // perform transpose as per the permute order
-    transpose_generic_nd_recursive(dstPtrTemp, dstStrides, srcPtrTemp, srcStrides, dstShape, tensorDims);
-}
+RppStatus transpose_f32_f32_host_tensor(Rpp32f *srcPtr,
+                                        RpptGenericDescPtr srcGenericDescPtr,
+                                        Rpp32f *dstPtr,
+                                        RpptGenericDescPtr dstGenericDescPtr,
+                                        Rpp32u *permTensor,
+                                        Rpp32u *roiTensor,
+                                        rpp::Handle& handle);
 
 template<typename T>
 RppStatus transpose_generic_host_tensor(T *srcPtr,
                                         RpptGenericDescPtr srcGenericDescPtr,
                                         T *dstPtr,
-                                        RpptGenericDescPtr dstGenericDescPtr,
-                                        Rpp32u *permTensor,
-                                        Rpp32u *roiTensor,
-                                        rpp::Handle& handle)
-{
-    Rpp32u numThreads = handle.GetNumThreads();
-    Rpp32u tensorDims = dstGenericDescPtr->numDims - 1;  // exclude batchsize from input dims
-    Rpp32u batchSize = dstGenericDescPtr->dims[0];
-
-    omp_set_dynamic(0);
-#pragma omp parallel for num_threads(numThreads)
-    for(int batchCount = 0; batchCount < batchSize; batchCount++)
-    {
-        T *srcPtrTemp, *dstPtrTemp;
-        srcPtrTemp = srcPtr + batchCount * srcGenericDescPtr->strides[0];
-        dstPtrTemp = dstPtr + batchCount * dstGenericDescPtr->strides[0];
-
-        // get the starting address of begin and length values from roiTensor
-        Rpp32u *roi = roiTensor + batchCount * tensorDims * 2;
-        Rpp32u *begin = roi;
-        Rpp32u *length = &roi[tensorDims];
-        Rpp32u *perm = permTensor;
-
-        bool copyInput = true;
-        for(int i = 0; i < tensorDims; i++)
-            copyInput *= (perm[i] == i);
-
-        // do memcpy of input to output since output order is same as input order
-        if(copyInput)
-        {
-            memcpy(dstPtrTemp, srcPtrTemp, (size_t)(srcGenericDescPtr->strides[0] * sizeof(T)));
-        }
-        else
-        {
-            for(int i = 1; i < tensorDims; i++)
-                srcPtrTemp += begin[i - 1] * srcGenericDescPtr->strides[i];
-            transpose_generic_setup_and_run(srcPtrTemp, dstPtrTemp, length, perm, tensorDims);
-        }
-    }
-
-    return RPP_SUCCESS;
-}
-
-RppStatus transpose_f32_f32_host_tensor(Rpp32f *srcPtr,
-                                        RpptGenericDescPtr srcGenericDescPtr,
-                                        Rpp32f *dstPtr,
                                         RpptGenericDescPtr dstGenericDescPtr,
                                         Rpp32u *permTensor,
                                         Rpp32u *roiTensor,
