7bb42ee9886a79b78659f772cb00033da29255fc
Srihari-mcw <srihari@multicorewareinc.com>
2025-03-22 17:34:17 +0530

    Update indentation inside ifdef flags

--- api/rpp.h
+++ api/rpp.h
@@ -51,11 +51,11 @@ typedef hipStream_t rppAcceleratorQueue_t;
 #endif
 
 #if __cplusplus >= 201703L && __has_include(<filesystem>)
-    #include <filesystem>
-    namespace fs = std::filesystem;
+#include <filesystem>
+namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
-    #include <experimental/filesystem>
-    namespace fs = std::experimental::filesystem;
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
 #endif
 
 /*! \brief Construct type name from a struct
--- src/include/common/tmp_dir.hpp
+++ src/include/common/tmp_dir.hpp
@@ -27,11 +27,11 @@ SOFTWARE.
 
 #include <string>
 #if __cplusplus >= 201703L && __has_include(<filesystem>)
-    #include <filesystem>
-    namespace fs = std::filesystem;
+#include <filesystem>
+namespace fs = std::filesystem;
 #elif __has_include(<experimental/filesystem>)
-    #include <experimental/filesystem>
-    namespace fs = std::experimental::filesystem;
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
 #endif
 
 namespace rpp {
