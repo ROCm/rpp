b9985d2c28df76dba6984fadb0cd520a1361ac2f
sampath117 <snehaa@multicorewareinc.com>
2025-05-28 13:11:14 -0400

    Added : doc image for Hue and Saturation

--- utilities/test_suite/common.py
+++ utilities/test_suite/common.py
@@ -127,7 +127,7 @@ miscAugmentationMap  = {
 }
 
 ImageAugmentationGroupMap = {
-    "color_augmentations" : [0, 1, 2, 3, 4, 13, 31, 34, 36, 45, 81],
+    "color_augmentations" : [0, 1, 2, 3, 4, 13, 31, 34, 36, 42, 43, 45, 81],
     "effects_augmentations" : [5, 6, 8, 10, 11, 29, 30, 32, 35, 46, 82, 83, 84],
     "geometric_augmentations" : [20, 21, 23, 24, 26, 28, 33, 37, 38, 39, 63, 79, 80, 92],
     "filter_augmentations" : [49, 54],
