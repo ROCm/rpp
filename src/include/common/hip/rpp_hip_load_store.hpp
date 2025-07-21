a4d6703fbefa242abe5d68ab06439e4f6c453412
Abishek <52214183+r-abishek@users.noreply.github.com>
2025-07-21 11:18:14 -0700

    Update rpp_hip_load_store.hpp

--- src/include/common/hip/rpp_hip_load_store.hpp
+++ src/include/common/hip/rpp_hip_load_store.hpp
@@ -2016,9 +2016,7 @@ struct FilterDispatchFloat
 {
     using SharedType = float;
     using SupportType = float3;
-
     __device__ __forceinline__ static void rpp_hip_load24_pkd3_to_pln3(T* src, float** dst) { rpp_hip_load24_pkd3_to_float24_pln3(src, dst); }
-
     __device__ __forceinline__ static void rpp_hip_load8(T* src, float* dst) { rpp_hip_load8_and_unpack_to_float8(src, (d_float8*)dst); }
 };
 
@@ -2027,15 +2025,10 @@ template <typename T>
 struct FilterDispatchChar
 {
     using SharedType = T;
-    // VectorType used to extract vector of values from the shared memory
-    using VectorType =  typename std::conditional<std::is_same<T, uchar>::value , uint3 , int3>::type;
-
+    using VectorType =  typename std::conditional<std::is_same<T, uchar>::value , uint3 , int3>::type; // VectorType used to extract vector of values from the shared memory
     __device__ __forceinline__ static void rpp_hip_load24_pkd3_to_pln3(uchar* src, uchar** dst) { rpp_hip_load24_pkd3_to_uchar8_pln3(src, dst); }
-
     __device__ __forceinline__ static void rpp_hip_load8(uchar* src, uchar* dst) { rpp_hip_load8_to_uchar8(src, dst); }
-
     __device__ __forceinline__ static void rpp_hip_load24_pkd3_to_pln3(schar* src, schar** dst) { rpp_hip_load24_pkd3_to_schar8_pln3(src, dst); }
-
     __device__ __forceinline__ static void rpp_hip_load8(schar* src, schar* dst) { rpp_hip_load8_to_schar8(src, dst); }
 };
 
