59e6da58db2c5493db10c998b23819d057c66676
Abishek <52214183+r-abishek@users.noreply.github.com>
2025-08-12 15:29:05 -0700

    Update flip.cpp

--- src/modules/tensor/cpu/kernel/flip.cpp
+++ src/modules/tensor/cpu/kernel/flip.cpp
@@ -344,6 +344,9 @@ RppStatus flip_f32_f32_host_tensor(Rpp32f *srcPtr,
         Rpp32u vFactor = roi.xywhROI.xy.y * srcDescPtr->strides.hStride;
         Rpp32s hStrideSrcIncrement = srcDescPtr->strides.hStride;
         constexpr Rpp32u RGB_CHANNELS = 3;     // Number of channels in RGB processing.
+        
+        // The subtraction of elementsToSkip (typically RGB_CHANNELS - 1) from hFlipFactor
+        // ensures correct alignment of the source pointer for the remaining elements.
         constexpr Rpp32u elementsToSkip = RGB_CHANNELS - 1;    // Skip two elements to align with the processing logic for RGB channels.
 
         // Initialize load functions with default values
