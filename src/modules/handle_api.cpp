5ac5162ad3870ffedbf422db65fca0261f7773ca
Maddisetty <hmaddise@ctr2-alola-login-01.amd.com>
2025-08-26 05:58:16 -0400

    Add handle deletes inside lambda function

--- src/modules/handle_api.cpp
+++ src/modules/handle_api.cpp
@@ -48,18 +48,16 @@ extern "C" rppStatus_t rppDestroy(rppHandle_t handle, RppBackend backend)
     if(backend == RppBackend::RPP_HOST_BACKEND)
     {
 #if GPU_SUPPORT
-        auto status = rpp::try_([&] { rpp::deref(handle).rpp_destroy_object_gpu(); });
+        auto status = rpp::try_([&] { rpp::deref(handle).rpp_destroy_object_gpu(); delete handle;});
 #else
-        auto status = rpp::try_([&] { rpp::deref(handle).rpp_destroy_object_host(); });
+        auto status = rpp::try_([&] { rpp::deref(handle).rpp_destroy_object_host(); delete handle;});
 #endif
-        delete handle;
         return status;
     }
 #if GPU_SUPPORT
     else if(backend == RppBackend::RPP_HIP_BACKEND || backend == RppBackend::RPP_OCL_BACKEND)
     {
-        auto status = rpp::try_([&] { rpp::deref(handle).rpp_destroy_object_gpu(); });
-        delete handle;
+        auto status = rpp::try_([&] { rpp::deref(handle).rpp_destroy_object_gpu(); delete handle;});
         return status;
     }
 #endif // GPU_SUPPORT
