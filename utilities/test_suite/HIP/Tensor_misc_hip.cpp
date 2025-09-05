28fbd0e5fa27ac6185cd5a9217506d2459a3e802
Srihari-mcw <srihari@multicorewareinc.com>
2025-09-05 18:37:18 +0530

    Update the compare_output calls

--- utilities/test_suite/HIP/Tensor_misc_hip.cpp
+++ utilities/test_suite/HIP/Tensor_misc_hip.cpp
@@ -466,7 +466,7 @@ int main(int argc, char **argv)
     if(qaMode)
     {
         CHECK_RETURN_STATUS(hipMemcpy(output, d_output, oBufferSizeInBytes, hipMemcpyDeviceToHost));
-        compare_output(output, nDim, batchSize, bitDepth, oBufferSize, dst, func, testCaseName, additionalParam, scriptPath, broadCastFlag, externalMeanStd);
+        compare_output(output, nDim, batchSize, bitDepth, oBufferSize, dst, func, testCaseName, additionalParam, scriptPath, broadCastCase ? broadCastFlag : 0, externalMeanStd);
     }
     else
     {
