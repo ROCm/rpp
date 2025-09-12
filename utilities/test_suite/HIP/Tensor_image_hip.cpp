f0e21956a3a5f338c83410b7606b5142a849599f
sampath117 <snehaa@multicorewareinc.com>
2025-09-12 03:30:07 -0400

    Removed space and review comments resolved

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -469,7 +469,6 @@ int main(int argc, char **argv)
         CHECK_RETURN_STATUS(hipHostMalloc(&maxTensor, batchSize * srcDescPtr->c * sizeof(Rpp32f)));
     }
 
-
     Rpp8u *posterizeLevelBits = nullptr;
     if(testCase == POSTERIZE)
         CHECK_RETURN_STATUS(hipHostMalloc(&posterizeLevelBits, batchSize * sizeof(Rpp8u)));
