13088e01e02d2cdd6ed62322c2f5f040aeb1c684
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2025-09-18 08:19:19 -0400

    Revert changes

--- src/modules/tensor/cpu/kernel/spectrogram.cpp
+++ src/modules/tensor/cpu/kernel/spectrogram.cpp
@@ -180,8 +180,8 @@ RppStatus spectrogram_host_tensor(Rpp32f *srcPtr,
         }
 
         // Set temporary buffers to 0
-        Rpp32f *fftInBuf  = static_cast<Rpp32f*>(_mm_malloc(fftInSize  * sizeof(Rpp32f), 32));
-        Rpp32f *fftOutBuf = static_cast<Rpp32f*>(_mm_malloc(fftOutSize * sizeof(Rpp32f), 32));
+        Rpp32f *fftInBuf = static_cast<Rpp32f*>(_mm_malloc(fftInSize * sizeof(Rpp32f), 32)); // ffts requires 32-byte aligned memory
+        Rpp32f *fftOutBuf = static_cast<Rpp32f*>(_mm_malloc(fftOutSize * sizeof(Rpp32f), 32)); // ffts requires 32-byte aligned memory
 
         for (Rpp32s w = 0; w < numWindows; w++)
         {
