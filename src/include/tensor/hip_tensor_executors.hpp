b8c089ce2dc5c1af75abedb47bf65b77f49088bb
Srihari-mcw <srihari@multicorewareinc.com>
2025-09-21 20:05:35 +0530

    Fix compilation issues

--- src/include/tensor/hip_tensor_executors.hpp
+++ src/include/tensor/hip_tensor_executors.hpp
@@ -104,12 +104,12 @@ RppStatus hip_exec_subtract_scalar_tensor(Rpp32f *srcPtr,
 
 // -------------------- tensor_arithmetic_operations --------------------
 
-template <typename T>
-RppStatus tensor_binary_arithmetic_op_dispatch_gpu_tensor(T *srcPtr1,
-                                                          T *srcPtr2,
+template <typename T1, typename T2>
+RppStatus tensor_binary_arithmetic_op_dispatch_gpu_tensor(T1 *srcPtr1,
+                                                          T1 *srcPtr2,
                                                           RpptGenericDescPtr srcPtr1GenericDescPtr,
                                                           RpptGenericDescPtr srcPtr2GenericDescPtr,
-                                                          T *dstPtr,
+                                                          T2 *dstPtr,
                                                           RpptGenericDescPtr dstGenericDescPtr,
                                                           RpptOp tensorOp,
                                                           RpptBroadcastMode broadcastMode,
