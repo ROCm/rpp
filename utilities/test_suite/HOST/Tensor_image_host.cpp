33d8dc987f6333df012b71fd7dcb3ee4d20a6817
Abishek <52214183+r-abishek@users.noreply.github.com>
2025-10-15 23:59:16 -0700

    minor bugfix

--- utilities/test_suite/HOST/Tensor_image_host.cpp
+++ utilities/test_suite/HOST/Tensor_image_host.cpp
@@ -1747,7 +1747,7 @@ int main(int argc, char **argv)
 
                     startWallTime = omp_get_wtime();
                     startCpuTime = clock();
-                    if (inputBitDepth == 0 || inputBitDepth == 1 || inputBitDepth == 2 || inputBitDepth == 5)
+                    if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
                         rppt_solarize_host(input, srcDescPtr, output, dstDescPtr, thresholdTensor, roiTensorPtrSrc, roiTypeSrc, handle);
                     else
                         missingFuncFlag = 1;
