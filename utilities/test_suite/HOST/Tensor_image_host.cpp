7a2d46f6978644dc926adb9fd7f42074fa963391
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2025-10-31 07:32:06 -0400

    Updated bin files for the next set of kernel F32 QA

--- utilities/test_suite/HOST/Tensor_image_host.cpp
+++ utilities/test_suite/HOST/Tensor_image_host.cpp
@@ -1668,7 +1668,7 @@ int main(int argc, char **argv)
 
                     if(srcDescPtr->c == 1)
                         reductionFuncResultArrLength = srcDescPtr->n;
-                    Rpp32f *mean = TensorMeanReferenceOutputs[inputChannels].data();
+                    Rpp32f *mean = TensorMeanReferenceOutputs_U8[inputChannels].data();
 
                     startWallTime = omp_get_wtime();
                     startCpuTime = clock();
@@ -1833,14 +1833,24 @@ int main(int argc, char **argv)
                 1.QA Flag is set
                 2.input bit depth 0 (U8)
                 3.source and destination layout are the same*/
-                if(qaFlag && BitDepthTestMode == U8_TO_U8 && (srcDescPtr->layout == dstDescPtr->layout) && !(randomOutputCase) && !(nonQACase))
+                if(qaFlag && ((BitDepthTestMode == U8_TO_U8) || (BitDepthTestMode == F32_TO_F32)) && (srcDescPtr->layout == dstDescPtr->layout) && !(randomOutputCase) && !(nonQACase))
                 {
                     if (testCase == TENSOR_SUM)
-                        compare_reduction_output(static_cast<uint64_t *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
+                    {
+                        if (BitDepthTestMode == U8_TO_U8)
+                            compare_reduction_output(static_cast<uint64_t *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
+                        else if (BitDepthTestMode == F32_TO_F32)
+                            compare_reduction_output(static_cast<Rpp32f *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
+                    }
                     else if (testCase == TENSOR_MEAN || testCase == TENSOR_STDDEV)
                         compare_reduction_output(static_cast<Rpp32f *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
                     else
-                        compare_reduction_output(static_cast<Rpp8u *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
+                    {
+                        if (BitDepthTestMode == U8_TO_U8)
+                            compare_reduction_output(static_cast<Rpp8u *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
+                        else if (BitDepthTestMode == F32_TO_F32)
+                            compare_reduction_output(static_cast<Rpp32f *>(reductionFuncResultArr), testCaseName, srcDescPtr, testCase, dst, scriptPath);
+                    }
                 }
             }
             else
--- utilities/test_suite/rpp_test_suite_image.h
+++ utilities/test_suite/rpp_test_suite_image.h
@@ -202,38 +202,74 @@ const unordered_set<int> noiseTypeCases = {NOISE};
 const unordered_set<int> pln1OutTypeCases = {COLOR_TO_GREYSCALE};
 
 // Golden outputs for Tensor min Kernel
-std::map<int, std::vector<Rpp8u>> TensorMinReferenceOutputs =
+std::map<int, std::vector<Rpp8u>> TensorMinReferenceOutputs_U8 =
 {
     {1, {1, 1, 7}},
     {3, {0, 0, 0, 0, 2, 0, 0, 0, 7, 9, 0, 0}}
 };
 
+// Golden outputs for Tensor min Kernel
+std::map<int, std::vector<Rpp32f>> TensorMinReferenceOutputs_F32 =
+{
+    {1, {0.004 , 0.004 , 0.027}},
+    {3, {0.000 , 0.000 , 0.000 , 0.000 , 0.008 , 0.000 , 0.000 , 0.000 , 0.027 , 0.035 , 0.000 , 0.000}}
+};
+
 // Golden outputs for Tensor max Kernel
-std::map<int, std::vector<Rpp8u>> TensorMaxReferenceOutputs =
+std::map<int, std::vector<Rpp8u>> TensorMaxReferenceOutputs_U8 =
 {
     {1, {239, 245, 255}},
     {3, {255, 240, 236, 255, 255, 242, 241, 255, 253, 255, 255, 255}}
 };
 
+// Golden outputs for Tensor max Kernel
+std::map<int, std::vector<Rpp32f>> TensorMaxReferenceOutputs_F32 =
+{
+    {1, {0.937 , 0.961 , 1.000}},
+    {3, {1.000 , 0.941 , 0.925 , 1.000 , 1.000 , 0.949 , 0.945 , 1.000 , 0.992 , 1.000 , 1.000 , 1.000}}
+};
+
 // Golden outputs for Tensor sum Kernel
-std::map<int, std::vector<uint64_t>> TensorSumReferenceOutputs =
+std::map<int, std::vector<uint64_t>> TensorSumReferenceOutputs_U8 =
 {
     {1, {334225, 813471, 2631125}},
     {3, {348380, 340992, 262616, 951988, 1056552, 749506, 507441, 2313499, 2170646, 2732368, 3320699, 8223713}}
 };
 
+// Golden outputs for Tensor sum Kernel
+std::map<int, std::vector<Rpp32f>> TensorSumReferenceOutputs_F32 =
+{
+    {1, {1310.686 , 3190.083 , 10318.138}},
+    {3, {1366.196 , 1337.224 , 1029.867 , 3733.286 , 4143.341 , 2939.239 , 1989.965 , 9072.546 , 8512.338 , 10715.169 , 13022.350 , 32249.857}}
+};
+
 // Golden outputs for Tensor mean Kernel
-std::map<int, std::vector<float>> TensorMeanReferenceOutputs =
+std::map<int, std::vector<Rpp32f>> TensorMeanReferenceOutputs_U8 =
 {
     {1, {133.690, 81.347, 116.939}},
     {3, {139.352, 136.397, 105.046, 126.932, 105.655, 74.951, 50.744, 77.117, 96.473, 121.439, 147.587, 121.833}}
 };
 
+// Golden outputs for Tensor mean Kernel
+std::map<int, std::vector<Rpp32f>> TensorMeanReferenceOutputs_F32 =
+{
+    {1, {0.524 , 0.319 , 0.459}},
+    {3, {0.546 , 0.535 , 0.412 , 0.498 , 0.414 , 0.294 , 0.199 , 0.302 , 0.378 , 0.476 , 0.579 , 0.478}}
+};
+
+
 // Golden outputs for Tensor stddev Kernel
-std::map<int, std::vector<float>> TensorStddevReferenceOutputs =
+std::map<int, std::vector<float>> TensorStddevReferenceOutputs_U8 =
 {
     {1, {49.583, 54.623, 47.649}},
     {3, {57.416, 47.901, 53.235, 55.220, 68.471, 55.735, 46.668, 61.880, 47.462, 49.039, 67.269, 59.130}}
+};
+
+// Golden outputs for Tensor stddev Kernel
+std::map<int, std::vector<Rpp32f>> TensorStddevReferenceOutputs_F32 =
+{
+    {1, {33957.297 , 20662.211 , 29702.545}},
+    {3, {35395.457 , 34644.871 , 26681.736 , 32240.775 , 26836.457 , 19037.635 , 12889.061 , 19587.814 , 24504.188 , 30845.547 , 37487.160 , 30945.639}}
 };
 
 template <typename T>
@@ -1249,9 +1285,14 @@ template <typename T>
 inline void compare_reduction_output(T* output, string funcName, RpptDescPtr srcDescPtr, int testCase, string dst, string scriptPath)
 {
     string func = funcName;
-    string dataType[4] = {"_u8_", "_f16_", "_f32_", "_i8_"};
-
-    func += dataType[srcDescPtr->dataType];
+    switch (srcDescPtr->dataType)
+    {
+        case RpptDataType::U8:  func += "_u8_"; break;
+        case RpptDataType::F16: func += "_f16_"; break;
+        case RpptDataType::F32: func += "_f32_"; break;
+        case RpptDataType::I8:  func += "_i8_"; break;
+        default:                func += "_unknown_"; break;
+    }
 
     if(srcDescPtr->layout == RpptLayout::NHWC)
         func += "Tensor_PKD3";
@@ -1266,19 +1307,36 @@ inline void compare_reduction_output(T* output, string funcName, RpptDescPtr src
     int fileMatch = 0;
     int matched_values = 0;
 
+    int inputBitDepth = srcDescPtr->dataType;
     T *refOutput;
     int numChannels = (srcDescPtr->c == 1) ? 1 : 3;
     int numOutputs = (srcDescPtr->c == 1) ? srcDescPtr->n : srcDescPtr->n * 4;
-    if(testCase == TENSOR_MIN)
-        refOutput = reinterpret_cast<T*>(TensorMinReferenceOutputs[numChannels].data());
-    else if(testCase == TENSOR_MAX)
-        refOutput = reinterpret_cast<T*>(TensorMaxReferenceOutputs[numChannels].data());
-    else if(testCase == TENSOR_SUM)
-        refOutput = reinterpret_cast<T*>(TensorSumReferenceOutputs[numChannels].data());
-    else if(testCase == TENSOR_MEAN)
-        refOutput = reinterpret_cast<T*>(TensorMeanReferenceOutputs[numChannels].data());
-    else if(testCase == TENSOR_STDDEV)
-        refOutput = reinterpret_cast<T*>(TensorStddevReferenceOutputs[numChannels].data());
+    if (inputBitDepth == RpptDataType::F32)
+    {
+        if (testCase == TENSOR_MIN)
+            refOutput = reinterpret_cast<T*>(TensorMinReferenceOutputs_F32[numChannels].data());
+        else if (testCase == TENSOR_MAX)
+            refOutput = reinterpret_cast<T*>(TensorMaxReferenceOutputs_F32[numChannels].data());
+        else if (testCase == TENSOR_SUM)
+            refOutput = reinterpret_cast<T*>(TensorSumReferenceOutputs_F32[numChannels].data());
+        else if (testCase == TENSOR_MEAN)
+            refOutput = reinterpret_cast<T*>(TensorMeanReferenceOutputs_F32[numChannels].data());
+        else if (testCase == TENSOR_STDDEV)
+            refOutput = reinterpret_cast<T*>(TensorStddevReferenceOutputs_F32[numChannels].data());
+    }
+    else if (inputBitDepth == RpptDataType::U8)
+    {
+        if (testCase == TENSOR_MIN)
+            refOutput = reinterpret_cast<T*>(TensorMinReferenceOutputs_U8[numChannels].data());
+        else if (testCase == TENSOR_MAX)
+            refOutput = reinterpret_cast<T*>(TensorMaxReferenceOutputs_U8[numChannels].data());
+        else if (testCase == TENSOR_SUM)
+            refOutput = reinterpret_cast<T*>(TensorSumReferenceOutputs_U8[numChannels].data());
+        else if (testCase == TENSOR_MEAN)
+            refOutput = reinterpret_cast<T*>(TensorMeanReferenceOutputs_U8[numChannels].data());
+        else if (testCase == TENSOR_STDDEV)
+            refOutput = reinterpret_cast<T*>(TensorStddevReferenceOutputs_U8[numChannels].data());
+    }
 
     if(srcDescPtr->c == 1)
     {
