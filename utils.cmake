15f6a55559b3c648d8ecd389fb3ce8e7655eba83
Jonathan Luu <jonatluu@amd.com>
2025-11-04 14:19:36 -0500

    use existing changelog. fix formatting

--- utils.cmake
+++ utils.cmake
@@ -24,34 +24,34 @@ function( configure__pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINT
 
       # Configure the changelog file
       configure_file(
-        "${CMAKE_SOURCE_DIR}/DEBIAN/changelog.in"
+        "${CMAKE_SOURCE_DIR}/CHANGELOG.md"
         "${CMAKE_BINARY_DIR}/DEBIAN/changelog.Debian"
         @ONLY
       )
 
       if( BUILD_ENABLE_LINTIAN_OVERRIDES )
-	if(DEFINED BUILD_SHARED_LIBS AND NOT ${BUILD_SHARED_LIBS} STREQUAL "")
-	  string(FIND ${DEB_OVERRIDES_INSTALL_FILENM} "static" OUT_VAR1)
-	  if(OUT_VAR1 EQUAL -1)
-	    set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-static" )
+        if(DEFINED BUILD_SHARED_LIBS AND NOT ${BUILD_SHARED_LIBS} STREQUAL "")
+          string(FIND ${DEB_OVERRIDES_INSTALL_FILENM} "static" OUT_VAR1)
+          if(OUT_VAR1 EQUAL -1)
+            set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-static" )
           endif()
-	else()
+        else()
           if(ENABLE_ASAN_PACKAGING)
-	    string( FIND ${DEB_OVERRIDES_INSTALL_FILENM} "asan" OUT_VAR2)
-	    if(OUT_VAR2 EQUAL -1)
-	      set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-asan" )
-	    endif()
+            string( FIND ${DEB_OVERRIDES_INSTALL_FILENM} "asan" OUT_VAR2)
+            if(OUT_VAR2 EQUAL -1)
+              set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-asan" )
+            endif()
           endif()
-	endif()
-	set( DEB_OVERRIDES_INSTALL_FILENM
-		"${DEB_OVERRIDES_INSTALL_FILENM}" CACHE STRING "Debian Package Lintian Override File Name" FORCE)
-        # Configure the changelog file
-        configure_file(
-          "${CMAKE_SOURCE_DIR}/DEBIAN/overrides.in"
-          "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}"
-	   FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
-          @ONLY
-        )
+        endif()
+        set( DEB_OVERRIDES_INSTALL_FILENM
+          "${DEB_OVERRIDES_INSTALL_FILENM}" CACHE STRING "Debian Package Lintian Override File Name" FORCE)
+              # Configure the changelog file
+              configure_file(
+                "${CMAKE_SOURCE_DIR}/DEBIAN/overrides.in"
+                "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}"
+          FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
+                @ONLY
+              )
       endif()
 
       # Install Change Log 
@@ -72,13 +72,13 @@ function( configure__pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINT
                   COMPONENT ${COMPONENT_NAME_T})
       endif()
 
-    # Install lintian overrides
-    if( BUILD_ENABLE_LINTIAN_OVERRIDES STREQUAL "ON" AND BUILD_DEBIAN_PKGING_FLAG STREQUAL "ON")
-      set( OVERRIDE_FILE "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}" )
-      install ( FILES ${OVERRIDE_FILE}
-          DESTINATION ${DEB_OVERRIDES_INSTALL_PATH}
-          COMPONENT ${COMPONENT_NAME_T})
-    endif()
+      # Install lintian overrides
+      if( BUILD_ENABLE_LINTIAN_OVERRIDES STREQUAL "ON" AND BUILD_DEBIAN_PKGING_FLAG STREQUAL "ON")
+        set( OVERRIDE_FILE "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}" )
+        install ( FILES ${OVERRIDE_FILE}
+            DESTINATION ${DEB_OVERRIDES_INSTALL_PATH}
+            COMPONENT ${COMPONENT_NAME_T})
+      endif()
 
     else()
         # License file
