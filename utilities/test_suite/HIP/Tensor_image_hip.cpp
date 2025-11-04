67429cdaa84c4b52546c2bc1e7100fc5c4c13d4b
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2025-11-04 08:24:22 -0500

    Updated bin files for jpeg_compression_distortion

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -1764,7 +1764,7 @@ int main(int argc, char **argv)
 
                     if(srcDescPtr->c == 1)
                         reductionFuncResultArrLength = srcDescPtr->n;
-                    memcpy(mean, TensorMeanReferenceOutputs[inputChannels].data(), sizeof(Rpp32f) * reductionFuncResultArrLength);
+                    memcpy(mean, TensorMeanReferenceOutputs_U8[inputChannels].data(), sizeof(Rpp32f) * reductionFuncResultArrLength);
 
                     startWallTime = omp_get_wtime();
                     if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
--- utilities/test_suite/common.py
+++ utilities/test_suite/common.py
@@ -129,7 +129,7 @@ imageAugmentationMap = {
     90: ["tensor_mean", "HOST", "HIP"],
     91: ["tensor_stddev", "HOST", "HIP"],
     92: ["slice", "HOST", "HIP"],
-    93: ["jpeg_compression_distortion", "HIP"],
+    93: ["jpeg_compression_distortion", "HOST", "HIP"],
     94: ["posterize", "HOST", "HIP"],
     95: ["solarize", "HOST", "HIP"]
 }
