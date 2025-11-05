a9e7ab1ee8900e283bdfbf760c865a2e9127f774
Srihari-mcw <srihari@multicorewareinc.com>
2025-11-05 12:03:11 +0530

    Modify the print statements

--- utilities/test_suite/HIP/Tensor_voxel_hip.cpp
+++ utilities/test_suite/HIP/Tensor_voxel_hip.cpp
@@ -399,7 +399,7 @@ int main(int argc, char * argv[])
             }
             if (errorCodeCapture != RPP_SUCCESS)
             {
-                cout << "\nThe functionality " << func << " returned an error status " << rppStatusToString[errorCodeCapture] << " on run number " << str(perfRunCount + 1) << " of " << str(numRuns) << " runs.\n";
+                cout << "\nThe functionality " << func << " returned an error status " << rppStatusToString[errorCodeCapture] << " on run number " << perfRunCount + 1 << " of " << numRuns << " runs.\n";
                 return errorCodeCapture;
             }
         }
