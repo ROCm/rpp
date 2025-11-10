15d47161e21cde3086d3361ff8a9c9ff749a072c
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2025-11-10 06:44:52 -0500

    Fix for Resize bilinear F32 QA HOST and HIP

--- src/modules/tensor/cpu/kernel/resize.cpp
+++ src/modules/tensor/cpu/kernel/resize.cpp
@@ -1638,8 +1638,8 @@ RppStatus resize_bilinear_f32_f32_host_tensor(Rpp32f *srcPtr,
         compute_dst_size_cap_host(&dstImgSize[batchCount], dstDescPtr);     // Check if the dstImgSize exceeds dst buffer size
         Rpp32f wRatio = ((Rpp32f)(roi.xywhROI.roiWidth)) / ((Rpp32f)(dstImgSize[batchCount].width));
         Rpp32f hRatio = ((Rpp32f)(roi.xywhROI.roiHeight)) / ((Rpp32f)(dstImgSize[batchCount].height));
-        Rpp32s maxHeightLimit = roi.xywhROI.roiHeight - 1;
-        Rpp32s maxWidthLimit = (roi.xywhROI.roiWidth - 1) * srcDescPtr->strides.wStride;
+        Rpp32s maxHeightLimit = roi.xywhROI.roiHeight - 2;
+        Rpp32s maxWidthLimit = (roi.xywhROI.roiWidth - 2) * srcDescPtr->strides.wStride;
         Rpp32s maxWidthLimitMinusStride = maxWidthLimit - srcDescPtr->strides.wStride;
         Rpp32s kernelSize = 2;
         Rpp32f kernelRadius = 1.0f; // kernelSize / 2
