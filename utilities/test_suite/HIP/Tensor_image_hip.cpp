5ea467766a5a82edb9af402c271ad6fa3d7e73bc
Srihari-mcw <srihari@multicorewareinc.com>
2025-12-01 15:22:11 +0530

    Fix test suite issues HIP

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -670,7 +670,7 @@ int main(int argc, char **argv)
                     }
 
                     startWallTime = omp_get_wtime();
-                    if (inputBitDepth == 0 || inputBitDepth == 1 || inputBitDepth == 2 || inputBitDepth == 5)
+                    if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
                         rppt_snow_gpu(d_input, srcDescPtr, d_output, dstDescPtr, brightnessCoefficient, snowThreshold, darkMode, roiTensorPtrSrc, roiTypeSrc, handle);
                     else
                         missingFuncFlag = 1;
