c973fc104f65e7d399043eef6cefdc77bb93f3ee
Srihari-mcw <srihari@multicorewareinc.com>
2025-12-03 10:00:55 +0530

    Rename function from hip_exec_roi_converison_ltrb_to_xywh to hip_exec_roi_conversion_ltrb_to_xywh

--- src/modules/tensor/hip/kernel/crop_and_patch.cpp
+++ src/modules/tensor/hip/kernel/crop_and_patch.cpp
@@ -326,7 +326,7 @@ RppStatus hip_exec_crop_and_patch_tensor(T *srcPtr1,
                                          rpp::Handle& handle)
 {
     if (roiType == RpptRoiType::LTRB)
-        hip_exec_roi_converison_ltrb_to_xywh(roiTensorPtrSrc, handle);
+        hip_exec_roi_conversion_ltrb_to_xywh(roiTensorPtrSrc, handle);
 
     int globalThreads_x = (dstDescPtr->w + 7) >> 3;
     int globalThreads_y = dstDescPtr->h;
