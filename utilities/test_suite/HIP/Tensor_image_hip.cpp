a441c9fa06e1a3a9943a363da651f3be0061ffb0
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2025-12-22 01:54:51 -0500

    Updated test suite

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -1861,13 +1861,13 @@ int main(int argc, char **argv)
 
                     Rpp32u boxesInEachImage = numGridsPerRow * numGridsPerColumn;
                     Rpp32u totalBoxes = srcDescPtr->n * boxesInEachImage;
-                    std::vector<RpptRoiLtrb> anchorBoxInfoTensor(totalBoxes);
+                    RpptRoiLtrb anchorBoxInfoTensor[totalBoxes];
                     Rpp32u maxHoleW = 0, maxHoleH = 0;
-                    init_grid_dropout(srcDescPtr->n, anchorBoxInfoTensor.data(), roiTensorPtrSrc, numGridsPerRow, numGridsPerColumn, maxHoleW, maxHoleH, holeRatio, randomOffset);
+                    init_grid_dropout(srcDescPtr->n, anchorBoxInfoTensor, roiTensorPtrSrc, numGridsPerRow, numGridsPerColumn, maxHoleW, maxHoleH, holeRatio, randomOffset);
 
                     startWallTime = omp_get_wtime();
                     if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
-                       errorCodeCapture = rppt_grid_dropout_gpu(d_input, srcDescPtr, d_output, dstDescPtr, anchorBoxInfoTensor.data(), boxesInEachImage, maxHoleW, maxHoleH, roiTensorPtrSrc, roiTypeSrc, handle);
+                       errorCodeCapture = rppt_grid_dropout_gpu(d_input, srcDescPtr, d_output, dstDescPtr, anchorBoxInfoTensor, boxesInEachImage, maxHoleW, maxHoleH, roiTensorPtrSrc, roiTypeSrc, handle);
                     else
                         missingFuncFlag = 1;
 
