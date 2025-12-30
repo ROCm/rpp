abd3979f3638e3d912034a187d5a1e2ea6eae68a
Srihari-mcw <srihari@multicorewareinc.com>
2025-12-30 09:16:48 +0530

    Fix compilation issues

--- src/modules/tensor/rppt_tensor_arithmetic_operations.cpp
+++ src/modules/tensor/rppt_tensor_arithmetic_operations.cpp
@@ -23,7 +23,7 @@ SOFTWARE.
 */
 
 #include "rppdefs.h"
-#include "rppi_validate.hpp"
+#include "rppt_validate.hpp"
 #include "rppt_tensor_arithmetic_operations.h"
 #include "host_tensor_executors.hpp"
 
