3e37e7282c90d69673225360fa5df8899ed4d255
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2026-02-17 20:41:48 -0600

    updated unified api for sobel filter

--- utilities/test_suite/HIP/Tensor_image_hip.cpp
+++ utilities/test_suite/HIP/Tensor_image_hip.cpp
@@ -1499,7 +1499,7 @@ int main(int argc, char **argv)
 
                     startWallTime = omp_get_wtime();
                     if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
-                        errorCodeCapture = rppt_sobel_filter_gpu(d_input, srcDescPtr, d_output, dstDescPtr, GradientType, kernelSize, roiTensorPtrSrc, roiTypeSrc, handle);
+                        errorCodeCapture = rppt_sobel_filter(d_input, srcDescPtr, d_output, dstDescPtr, GradientType, kernelSize, roiTensorPtrSrc, roiTypeSrc, handle, RppBackend::RPP_HIP_BACKEND);
                     else
                         missingFuncFlag = 1;
 
--- utilities/test_suite/HOST/Tensor_image_host.cpp
+++ utilities/test_suite/HOST/Tensor_image_host.cpp
@@ -1356,7 +1356,7 @@ int main(int argc, char **argv)
                     startWallTime = omp_get_wtime();
                     startCpuTime = clock();
                     if (BitDepthTestMode == U8_TO_U8 || BitDepthTestMode == F16_TO_F16 || BitDepthTestMode == F32_TO_F32 || BitDepthTestMode == I8_TO_I8)
-                        rppt_sobel_filter_host(input, srcDescPtr, output, dstDescPtr, GradientType, kernelSize, roiTensorPtrSrc, roiTypeSrc, handle);
+                        errorCodeCapture = rppt_sobel_filter(input, srcDescPtr, output, dstDescPtr, GradientType, kernelSize, roiTensorPtrSrc, roiTypeSrc, handle, RppBackend::RPP_HOST_BACKEND);
                     else
                         missingFuncFlag = 1;
 
