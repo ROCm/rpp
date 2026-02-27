53b28ecf6cf4e6b2949f05ae420d4be6bbf0cc51
HazarathKumarM <hazarathkumar@multicorewareinc.com>
2026-02-27 03:36:56 -0600

    Add unified api for arithmetic operators

--- api/rppt_tensor_arithmetic_operations.h
+++ api/rppt_tensor_arithmetic_operations.h
@@ -177,173 +177,89 @@ RppStatus rppt_log(RppPtr_t srcPtr, RpptGenericDescPtr srcGenericDescPtr, RppPtr
  */
 RppStatus rppt_log1p(RppPtr_t srcPtr, RpptGenericDescPtr srcGenericDescPtr, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, Rpp32u *roiTensor, rppHandle_t rppHandle, RppBackend executionBackend);
 
-/*! \brief Tensor Add Tensor operation on HOST backend with tensor broadcasting support
+/*! \brief Tensor Add Tensor operation on HIP/HOST backend with tensor broadcasting support
  * \details Performs element-wise addition of two N-dimensional tensors.
  *          For every axis, the two input tensors must either have the same length or one of them must be 1.
  *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HOST memory
- * \param [in] srcPtr2 source2 tensor in HOST memory
+ * \param [in] srcPtr1 source1 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
+ * \param [in] srcPtr2 source2 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HOST memory
+ * \param [in] srcGenericDescPtr2 source2 tensor descriptor
+ * \param [out] dstPtr destination tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] dstGenericDescPtr destination tensor descriptor
  * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
  * \param [in] roiTensor1 values to represent dimensions of first input tensor
  * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] rppHandle RPP HIP/HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] executionBackend Backend type (RPP_HOST_BACKEND or RPP_HIP_BACKEND)
  * \return A <tt> \ref RppStatus</tt> enumeration.
  * \retval RPP_SUCCESS Successful completion.
  * \retval RPP_ERROR* Unsuccessful completion.
  */
-RppStatus rppt_tensor_add_tensor_host(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
+RppStatus rppt_tensor_add_tensor(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle, RppBackend executionBackend);
 
-#ifdef GPU_SUPPORT
-/*! \brief Tensor Add Tensor operation on HIP backend with tensor broadcasting support
- * \details Performs element-wise addition of two N-dimensional tensors.
- *          For every axis, the two input tensors must either have the same length or one of them must be 1.
- *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HIP memory
- * \param [in] srcPtr2 source2 tensor in HIP memory
- * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HIP memory
- * \param [in] dstGenericDescPtr destination tensor descriptor
- * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
- * \param [in] roiTensor1 values to represent dimensions of first input tensor
- * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HIP handle created with <tt>\ref rppCreate()</tt>
- * \return A <tt> \ref RppStatus</tt> enumeration.
- * \retval RPP_SUCCESS Successful completion.
- * \retval RPP_ERROR* Unsuccessful completion.
- */
-RppStatus rppt_tensor_add_tensor_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
-#endif
-
-/*! \brief Tensor Subtract Tensor operation on HOST backend with tensor broadcasting support
+/*! \brief Tensor Subtract Tensor operation on HIP/HOST backend with tensor broadcasting support
  * \details Performs element-wise subtraction of two N-dimensional tensors.
  *          For every axis, the two input tensors must either have the same length or one of them must be 1.
  *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HOST memory
- * \param [in] srcPtr2 source2 tensor in HOST memory
+ * \param [in] srcPtr1 source1 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
+ * \param [in] srcPtr2 source2 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HOST memory
+ * \param [in] srcGenericDescPtr2 source2 tensor descriptor
+ * \param [out] dstPtr destination tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] dstGenericDescPtr destination tensor descriptor
  * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
  * \param [in] roiTensor1 values to represent dimensions of first input tensor
  * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] rppHandle RPP HIP/HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] executionBackend Backend type (RPP_HOST_BACKEND or RPP_HIP_BACKEND)
  * \return A <tt> \ref RppStatus</tt> enumeration.
  * \retval RPP_SUCCESS Successful completion.
  * \retval RPP_ERROR* Unsuccessful completion.
  */
-RppStatus rppt_tensor_subtract_tensor_host(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
+RppStatus rppt_tensor_subtract_tensor(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle, RppBackend executionBackend);
 
-#ifdef GPU_SUPPORT
-/*! \brief Tensor Subtract Tensor operation on HIP backend with tensor broadcasting support
- * \details Performs element-wise subtraction of two N-dimensional tensors.
- *          For every axis, the two input tensors must either have the same length or one of them must be 1.
- *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HIP memory
- * \param [in] srcPtr2 source2 tensor in HIP memory
- * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HIP memory
- * \param [in] dstGenericDescPtr destination tensor descriptor
- * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
- * \param [in] roiTensor1 values to represent dimensions of first input tensor
- * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HIP handle created with <tt>\ref rppCreate()</tt>
- * \return A <tt> \ref RppStatus</tt> enumeration.
- * \retval RPP_SUCCESS Successful completion.
- * \retval RPP_ERROR* Unsuccessful completion.
- */
-RppStatus rppt_tensor_subtract_tensor_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
-#endif
-
-/*! \brief Tensor Multiply Tensor operation on HOST backend with tensor broadcasting support
+/*! \brief Tensor Multiply Tensor operation on HIP/HOST backend with tensor broadcasting support
  * \details Performs element-wise multiplication of two N-dimensional tensors.
  *          For every axis, the two input tensors must either have the same length or one of them must be 1.
  *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HOST memory
- * \param [in] srcPtr2 source2 tensor in HOST memory
+ * \param [in] srcPtr1 source1 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
+ * \param [in] srcPtr2 source2 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HOST memory
+ * \param [in] srcGenericDescPtr2 source2 tensor descriptor
+ * \param [out] dstPtr destination tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] dstGenericDescPtr destination tensor descriptor
  * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
  * \param [in] roiTensor1 values to represent dimensions of first input tensor
  * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] rppHandle RPP HIP/HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] executionBackend Backend type (RPP_HOST_BACKEND or RPP_HIP_BACKEND)
  * \return A <tt> \ref RppStatus</tt> enumeration.
  * \retval RPP_SUCCESS Successful completion.
  * \retval RPP_ERROR* Unsuccessful completion.
  */
-RppStatus rppt_tensor_multiply_tensor_host(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
+RppStatus rppt_tensor_multiply_tensor(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle, RppBackend executionBackend);
 
-#ifdef GPU_SUPPORT
-/*! \brief Tensor Multiply Tensor operation on HIP backend with tensor broadcasting support
- * \details Performs element-wise multiplication of two N-dimensional tensors.
- *          For every axis, the two input tensors must either have the same length or one of them must be 1.
- *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HIP memory
- * \param [in] srcPtr2 source2 tensor in HIP memory
- * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HIP memory
- * \param [in] dstGenericDescPtr destination tensor descriptor
- * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
- * \param [in] roiTensor1 values to represent dimensions of first input tensor
- * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HIP handle created with <tt>\ref rppCreate()</tt>
- * \return A <tt> \ref RppStatus</tt> enumeration.
- * \retval RPP_SUCCESS Successful completion.
- * \retval RPP_ERROR* Unsuccessful completion.
- */
-RppStatus rppt_tensor_multiply_tensor_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
-#endif
-
-/*! \brief Tensor Divide Tensor operation on HOST backend with tensor broadcasting support
+/*! \brief Tensor Divide Tensor operation on HIP/HOST backend with tensor broadcasting support
  * \details Performs element-wise division of two N-dimensional tensors.
  *          For every axis, the two input tensors must either have the same length or one of them must be 1.
  *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HOST memory
- * \param [in] srcPtr2 source2 tensor in HOST memory
+ * \param [in] srcPtr1 source1 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
+ * \param [in] srcPtr2 source2 tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HOST memory
+ * \param [in] srcGenericDescPtr2 source2 tensor descriptor
+ * \param [out] dstPtr destination tensor in HIP memory (for HIP backend) or HOST memory (for HOST backend)
  * \param [in] dstGenericDescPtr destination tensor descriptor
  * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
  * \param [in] roiTensor1 values to represent dimensions of first input tensor
  * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] rppHandle RPP HIP/HOST handle created with <tt>\ref rppCreate()</tt>
+ * \param [in] executionBackend Backend type (RPP_HOST_BACKEND or RPP_HIP_BACKEND)
  * \return A <tt> \ref RppStatus</tt> enumeration.
  * \retval RPP_SUCCESS Successful completion.
  * \retval RPP_ERROR* Unsuccessful completion.
  */
-RppStatus rppt_tensor_divide_tensor_host(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
-
-#ifdef GPU_SUPPORT
-/*! \brief Tensor Divide Tensor operation on HIP backend with tensor broadcasting support
- * \details Performs element-wise Division of two N-dimensional tensors.
- *          For every axis, the two input tensors must either have the same length or one of them must be 1.
- *          DISABLE_BROADCAST can be chosen as broadcastMode only when every sample in the batch has identical dimensions.
- * \param [in] srcPtr1 source1 tensor in HIP memory
- * \param [in] srcPtr2 source2 tensor in HIP memory
- * \param [in] srcGenericDescPtr1 source1 tensor descriptor
- * \param [in] srcGenericDescPtr1 source2 tensor descriptor
- * \param [out] dstPtr destination tensor in HIP memory
- * \param [in] dstGenericDescPtr destination tensor descriptor
- * \param [in] broadcastMode enum to represent broadcasting mode is disabled or not, can be set based on input tensor shape
- * \param [in] roiTensor1 values to represent dimensions of first input tensor
- * \param [in] roiTensor2 values to represent dimensions of second input tensor
- * \param [in] rppHandle RPP HIP handle created with <tt>\ref rppCreate()</tt>
- * \return A <tt> \ref RppStatus</tt> enumeration.
- * \retval RPP_SUCCESS Successful completion.
- * \retval RPP_ERROR* Unsuccessful completion.
- */
-RppStatus rppt_tensor_divide_tensor_gpu(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle);
-#endif
+RppStatus rppt_tensor_divide_tensor(RppPtr_t srcPtr1, RppPtr_t srcPtr2, RpptGenericDescPtr srcGenericDescPtr1, RpptGenericDescPtr srcGenericDescPtr2, RppPtr_t dstPtr, RpptGenericDescPtr dstGenericDescPtr, RpptBroadcastMode broadcastMode, Rpp32u *roiTensor1, Rpp32u *roiTensor2, rppHandle_t rppHandle, RppBackend executionBackend);
 
 /*! @}
  */
