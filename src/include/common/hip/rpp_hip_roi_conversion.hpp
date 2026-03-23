7b77af3096faabaa94c84fbb9e8e2335e65ecbaa
Zach Vincze <zavincze@amd.com>
2026-03-23 18:00:46 -0400

    Fix typo in function name

--- src/include/common/hip/rpp_hip_roi_conversion.hpp
+++ src/include/common/hip/rpp_hip_roi_conversion.hpp
@@ -27,9 +27,11 @@ SOFTWARE.
 
 #include <hip/hip_runtime.h>
 
+// TODO: Remove [[maybe_unused]] once these helpers are used in the codebase.
+
 // LTRB to XYWH
 
-[[maybe_unused]] static __global__ void roi_converison_ltrb_to_xywh(int *roiTensorPtrSrc)
+[[maybe_unused]] static __global__ void roi_conversion_ltrb_to_xywh(int *roiTensorPtrSrc)
 {
     int id_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 4;
 
@@ -50,7 +52,7 @@ SOFTWARE.
     int globalThreads_y = 1;
     int globalThreads_z = 1;
 
-    hipLaunchKernelGGL(roi_converison_ltrb_to_xywh,
+    hipLaunchKernelGGL(roi_conversion_ltrb_to_xywh,
                        dim3(ceil((float)globalThreads_x/localThreads_x), ceil((float)globalThreads_y/localThreads_y), ceil((float)globalThreads_z/localThreads_z)),
                        dim3(localThreads_x, localThreads_y, localThreads_z),
                        0,
@@ -63,7 +65,7 @@ SOFTWARE.
 
 // XYWH to LTRB
 
-[[maybe_unused]] static __global__ void roi_converison_xywh_to_ltrb(int *roiTensorPtrSrc)
+[[maybe_unused]] static __global__ void roi_conversion_xywh_to_ltrb(int *roiTensorPtrSrc)
 {
     int id_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 4;
 
@@ -84,7 +86,7 @@ SOFTWARE.
     int globalThreads_y = 1;
     int globalThreads_z = 1;
 
-    hipLaunchKernelGGL(roi_converison_xywh_to_ltrb,
+    hipLaunchKernelGGL(roi_conversion_xywh_to_ltrb,
                        dim3(ceil((float)globalThreads_x/localThreads_x), ceil((float)globalThreads_y/localThreads_y), ceil((float)globalThreads_z/localThreads_z)),
                        dim3(localThreads_x, localThreads_y, localThreads_z),
                        0,
