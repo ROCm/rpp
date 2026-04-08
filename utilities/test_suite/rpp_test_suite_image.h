09ac2e25576707a0835edb32ef3dd658d31cf74f
Lakshmi Kumar <lakshmi.kumar@amd.com>
2026-04-08 10:09:32 -0700

    Update rpp_test_suite_image.h

--- utilities/test_suite/rpp_test_suite_image.h
+++ utilities/test_suite/rpp_test_suite_image.h
@@ -1978,7 +1978,7 @@ void generate_channel_dropout_mask(Rpp8u* dropoutTensor, Rpp32f* dropoutProbabil
 }
 
 // Dropout Region initializer for unit and performance testing
-void inline init_cutout_dropout(int batchSize, int maxBoxesPerImage, Rpp32u* numOfBoxes, RpptRoiLtrb* anchorBoxInfoTensor, RpptROIPtr roiTensorPtrSrc, int channels, int inputBitDepth, int seed, int dropoutType, void *colorBuffer = NULL)
+void inline init_cutout_dropout(int batchSize, int maxBoxesPerImage, Rpp32u* numOfBoxes, RpptRoiLtrb* anchorBoxInfoTensor, RpptROIPtr roiTensorPtrSrc, int channels, int BitDepthTestMode, int seed, int dropoutType, void *colorBuffer = NULL)
 {
     std::mt19937 rng(seed);
     std::uniform_real_distribution<float> pos_ratio(0.1f, 0.9f);
@@ -2015,13 +2015,13 @@ void inline init_cutout_dropout(int batchSize, int maxBoxesPerImage, Rpp32u* num
             int colorOffset = (i * maxBoxesPerImage) * channels;
             Rpp32f dropoutColor = 0.0f;
             for (int c = 0; c < channels; c++)
-                if (inputBitDepth == 0)
+                if (BitDepthTestMode == U8_TO_U8)
                     colors8u[colorOffset + c] = (Rpp8u)dropoutColor;
-                else if (inputBitDepth == 2)
+                else if (BitDepthTestMode == F16_TO_F16)
                     colors16f[colorOffset + c] = (Rpp16f)(dropoutColor * ONE_OVER_255);
-                else if (inputBitDepth == 1)
+                else if (BitDepthTestMode == F32_TO_F32)
                     colors32f[colorOffset + c] = (Rpp32f)(dropoutColor);
-                else if (inputBitDepth == 3)
+                else if (BitDepthTestMode == I8_TO_I8)
                     colors8s[colorOffset + c] = (Rpp8s)(dropoutColor - 128);
         }
         numOfBoxes[i] = 1;
