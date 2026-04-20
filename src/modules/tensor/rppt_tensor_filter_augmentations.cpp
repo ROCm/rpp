7b10524b07d3b63b4b21f954f9749e274c983b6e
Lakshmi Kumar <lakshmi.kumar@amd.com>
2026-04-20 11:17:00 -0700

    Update rppt_tensor_filter_augmentations.cpp

--- src/modules/tensor/rppt_tensor_filter_augmentations.cpp
+++ src/modules/tensor/rppt_tensor_filter_augmentations.cpp
@@ -566,8 +566,7 @@ RppStatus rppt_sobel_filter(RppPtr_t srcPtr,
         {
             size_t elementSize = (srcDescPtr->dataType == RpptDataType::F32) ? 4 : 
                                     (srcDescPtr->dataType == RpptDataType::F16) ? 2 : 1;
-            size_t dataSize = static_cast<size_t>(dstDescPtr->offsetInBytes) +
-                               static_cast<size_t>(dstDescPtr->strides.nStride) * static_cast<size_t>(dstDescPtr->n) * elementSize;
+            size_t dataSize = dstDescPtr->strides.nStride * dstDescPtr->n * elementSize;
             CHECK_RETURN_STATUS(hipMalloc(&tempPtr, dataSize));
 
             RpptSubpixelLayout srcSubpixelLayout = RpptSubpixelLayout::RGBtype;
