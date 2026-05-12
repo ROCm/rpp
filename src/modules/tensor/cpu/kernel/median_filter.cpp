ece3600c601478da796e0b33259ff912f79c6085
Manjunath Siddaiah <163046790+msiddaiah@users.noreply.github.com>
2026-05-12 16:50:23 -0500

    Fix for host median filter QA failures

--- src/modules/tensor/cpu/kernel/median_filter.cpp
+++ src/modules/tensor/cpu/kernel/median_filter.cpp
@@ -1503,7 +1503,7 @@ RppStatus median_filter_generic_host_tensor(T *srcPtr,
                 }
             }
         }
-        else if ((srcDescPtr->layout == RpptLayout::NCHW) && (dstDescPtr->layout == RpptLayout::NHWC))
+        else if ((srcDescPtr->layout == RpptLayout::NHWC) && (dstDescPtr->layout == RpptLayout::NHWC))
         {
 #if __AVX2__
             // Use simplified histogram method for U8 PKD with large kernels
@@ -1565,6 +1565,32 @@ RppStatus median_filter_generic_host_tensor(T *srcPtr,
                     }
                     dstPtrRow += dstDescPtr->strides.hStride;
                 }
+            }
+        }
+        else if ((srcDescPtr->layout == RpptLayout::NCHW) && (dstDescPtr->layout == RpptLayout::NHWC))
+        {
+            T *dstPtrRow = dstPtrChannel;
+            for (Rpp32s i = 0; i < roi.xywhROI.roiHeight; i++)
+            {
+                T *dstPtrTemp = dstPtrRow;
+                for (Rpp32s j = 0; j < roi.xywhROI.roiWidth; j++)
+                {
+                    T *dstPtrTempChn = dstPtrTemp;
+                    T *srcPtrTempChn = srcPtrChannel;
+                    for (Rpp32s c = 0; c < srcDescPtr->c; c++)
+                    {
+                        if (useSortNet3)
+                            median_filter_3x3_sortnet(srcPtrTempChn, dstPtrTempChn, i, j, roi.xywhROI.roiHeight - 1, roi.xywhROI.roiWidth - 1, 1, srcDescPtr);
+                        else if (useSortNet5)
+                            median_filter_5x5_sortnet(srcPtrTempChn, dstPtrTempChn, i, j, roi.xywhROI.roiHeight - 1, roi.xywhROI.roiWidth - 1, 1, srcDescPtr);
+                        else
+                            median_filter_generic(srcPtrTempChn, dstPtrTempChn, i, j, kernelSizeSquared, padLength, roi.xywhROI.roiHeight - 1, roi.xywhROI.roiWidth - 1, 1, srcDescPtr, dstDescPtr);
+                        srcPtrTempChn += srcDescPtr->strides.cStride;
+                        dstPtrTempChn++;
+                    }
+                    dstPtrTemp += dstDescPtr->c;
+                }
+                dstPtrRow += dstDescPtr->strides.hStride;
             }
         }
         else if ((srcDescPtr->layout == RpptLayout::NHWC) && (dstDescPtr->layout == RpptLayout::NCHW))
