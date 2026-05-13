3b8b8ed70c9332e5925f81c61c99f2e8a7943c25
LakshmiKumar23 <lakshmi.kumar@amd.com>
2026-05-13 11:42:05 -0700

    make pandas optional for qa mode

--- utilities/test_suite/common.py
+++ utilities/test_suite/common.py
@@ -27,7 +27,6 @@ import argparse
 import sys
 import datetime
 import shutil
-import pandas as pd
 import signal
 from enum import Enum
 
@@ -417,7 +416,7 @@ def print_qa_tests_summary(qaFilePath, supportedCaseList, nonQACaseList, fileNam
         resultsInfo = "\n\nFinal Results of Tests:"
         resultsInfo += "\n    - Total test cases including all subvariants REQUESTED = " + str(numLines)
         resultsInfo += "\n    - Total test cases including all subvariants PASSED = " + str(numPassed)
-        resultsInfo += "\n\nGeneral information on Tensor voxel test suite availability:"
+        resultsInfo += "\n\nGeneral information on " + fileName + " test suite availability:"
         resultsInfo += "\n    - Total augmentations supported in Tensor test suite = " + str(len(supportedCaseList))
         resultsInfo += "\n    - Total augmentations with golden output QA test support = " + str(len(supportedCaseList) - len(nonQACaseList))
         resultsInfo += "\n    - Total augmentations without golden output QA test support (due to randomization involved) = " + str(len(nonQACaseList))
